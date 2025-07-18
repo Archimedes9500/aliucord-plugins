2754ec6fe8dd821c362e6d4c4ff81dc69d49763b
Archimedes5000 <archimedes9500@gmail.com>
2025-07-18 21:44:32 +0200

    fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "AliucordPlugins"
-include(":plugins:AudioPlayer", ":plugins:MyFirstCommand", ":plugins:MyFirstPatch", ":plugins:Shit", )
+include("plugins:AudioPlayer", "plugins:MyFirstCommand", "plugins:MyFirstPatch", "plugins:Shit", )
 
