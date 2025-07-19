c39594645998e1b830a3dcbe192f492fdcabd0a0
GitHub Action <action@github.com>
2025-07-19 18:54:36 +0000

    Clone files from foreign repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 rootProject.name = "AliucordPlugins"
 
+include(":AnchorMessage")
+project(":AnchorMessage").projectDir = file("./plugins/AnchorMessage")
 include(":MyFirstCommand")
 project(":MyFirstCommand").projectDir = file("./plugins/MyFirstCommand")
 include(":MyFirstPatch")
 project(":MyFirstPatch").projectDir = file("./plugins/MyFirstPatch")
-include(":Shit")
-project(":Shit").projectDir = file("./plugins/Shit")
 
