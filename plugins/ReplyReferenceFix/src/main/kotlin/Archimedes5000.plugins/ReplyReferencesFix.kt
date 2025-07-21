a69fd92c4918a1e5dddec2107282433a38f2fae2
GitHub Action <action@github.com>
2025-07-21 20:40:23 +0000

    Clone files from foreign repository

--- plugins/ReplyReferenceFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
+++ plugins/ReplyReferenceFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
@@ -10,11 +10,20 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 
+import org.json.JSONObject
+import com.aliucord.Http
+import com.google.gson.Gson
+import java.net.URL
+
 @AliucordPlugin(requiresRestart = false)
 class ReplyReferencesFix: Plugin(){
 	@SuppressLint("SetTextI18n")
 	override fun start(context: Context){
-		
+		var response = try{
+			URL("https://www.mediawiki.org/w/api.php?action=query?format=json").readText();
+		};
+		response = new JSONObject(response);
+		showToast(response["batchcomplete"], showLonger = false);
 	}
 	override fun stop(context: Context) = patcher.unpatchAll();
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,4 +8,6 @@ include(":MyFirstCommand")
 project(":MyFirstCommand").projectDir = file("./plugins/MyFirstCommand")
 include(":MyFirstPatch")
 project(":MyFirstPatch").projectDir = file("./plugins/MyFirstPatch")
+include(":ReplyReferenceFix")
+project(":ReplyReferenceFix").projectDir = file("./plugins/ReplyReferenceFix")
 
