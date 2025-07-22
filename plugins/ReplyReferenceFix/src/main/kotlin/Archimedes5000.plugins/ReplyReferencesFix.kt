646910e175300a98e80120de6f7d484dba66607b
GitHub Action <action@github.com>
2025-07-22 00:12:09 +0000

    Clone files from foreign repository

--- plugins/ReplyReferenceFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
+++ plugins/ReplyReferenceFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
@@ -11,24 +11,33 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 
-import org.json.JSONObject
-import com.aliucord.Http
-import com.google.gson.Gson
 import java.net.URL
+import java.net.HttpURLConnection
+import java.io.OutputStream
+import org.json.JSONObject
 
 @AliucordPlugin(requiresRestart = false)
 class ReplyReferencesFix: Plugin(){
 	@SuppressLint("SetTextI18n")
 	override fun start(context: Context){
-		var response = try{
-			URL("https://www.mediawiki.org/w/api.php?action=query?format=json")
-				.getContent()
-			;
-		}catch(e: Exception){
-			e.printStackTrace();
-		};
-		response = JSONObject(response);
-		showToast(response.get("batchcomplete").toString(), showLonger = false);
+		val link = URL("https://mediawiki.org/w/api.php");
+		val r = link.openConnection() as HttpURLConnection;
+		r.requestMethod = "POST";
+		r.doOutput = true;
+		r.setRequestProperty(
+			"Content-Type",
+			"application/x-www-form-urlencoded"
+		);
+		var content = "action=query&format=json&formatversion=2";
+		val body = request.outputStream as OutputStream;
+		body.write(content.toByteArray());
+		body.flush();
+		var response = r.inputStream
+			.bufferedReader()
+			.readText()
+		;
+		var j = JSONObject(response);
+		showToast(j.opt("batchcomplete"), showLonger = false);
 	}
 	override fun stop(context: Context) = patcher.unpatchAll();
 }
