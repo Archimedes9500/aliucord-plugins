c0fa9ba7ecdd3063d6a8260a42e03c2e2a03a041
Archimedes5000 <archimedes9500@gmail.com>
2025-07-26 13:31:28 +0200

    fix

--- plugins/ReplyReferencesFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
+++ plugins/ReplyReferencesFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
@@ -15,49 +15,43 @@ import com.aliucord.Logger
 class ReplyReferencesFix:Plugin(){
 	@SuppressLint("SetTextI18n")
 	override fun start(context:Context){
-		with(WidgetChatListAdapterItemMessage::class.java){
-			patcher.patch( //setting listeners
-				getDeclaredMethod(
-					"onConfigure",
-					ChatListEntry::class.java
-				),
-				Hook{
-					frame ->
-					val item = frame.thisObject as WidgetChatListAdapterItemMessage;
-					val replyViewID = Utils.getResId(
-						"chat_list_adapter_item_text_decorator",
-						"id"
-					);
-					val iconViewID = Utils.getResId(
-						"chat_list_adapter_item_text_decorator_reply_link_icon",
-						"id"
-					);
-					Logger().debug(frame.toString());
-					/*if(
-						item.itemView.id == replyViewID
-						|| item.itemView.id == iconViewID
-					){
-						item.itemView.setOnClickListener{
-							try{
-								var msg =
-									(
-										frame.args.message
-										as WidgetChatListAdapterItemMessage
-									)
-								;
-								var t = msg.messageReference;
-								StoreStream.getMessagesLoader()
-									.jumpToMessage(t.channelID, t.messageID)
-								;
-							}catch(e:IllegalAccessException){
-								e.printStackTrace();
-							}catch(e:InvocationTargetException){
-								e.printStackTrace();
-							}
-						}
-					}*/
+		patcher.after<WidgetChatListAdapterItemMessage>(
+            "onConfigure",
+            Int::class.java,
+            ChatListEntry::class.java
+        ){
+			val replyViewID = Utils.getResId(
+				"chat_list_adapter_item_text_decorator",
+				"id"
+			);
+			val iconViewID = Utils.getResId(
+				"chat_list_adapter_item_text_decorator_reply_link_icon",
+				"id"
+			);
+			Logger().debug(this.toString());
+			/*if(
+				item.itemView.id == replyViewID
+				|| item.itemView.id == iconViewID
+			){
+				item.itemView.setOnClickListener{
+					try{
+						var msg =
+							(
+								frame.args.message
+								as WidgetChatListAdapterItemMessage
+							)
+						;
+						var t = msg.messageReference;
+						StoreStream.getMessagesLoader()
+							.jumpToMessage(t.channelID, t.messageID)
+						;
+					}catch(e:IllegalAccessException){
+						e.printStackTrace();
+					}catch(e:InvocationTargetException){
+						e.printStackTrace();
+					}
 				}
-			)
+			}*/
 		}
 	}
 	override fun stop(context:Context) = patcher.unpatchAll();
