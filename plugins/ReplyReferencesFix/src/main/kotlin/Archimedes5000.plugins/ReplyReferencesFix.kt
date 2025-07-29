838a096d612b29d265b2eb1953a9d08a305f24d3
GitHub Action <action@github.com>
2025-07-29 21:06:26 +0000

    Clone files from foreign repository

--- plugins/ReplyReferencesFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
+++ plugins/ReplyReferencesFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
@@ -8,7 +8,8 @@ import com.discord.widgets.chat.list.adapter.`WidgetChatListAdapterItemMessage$c
 import com.aliucord.patcher.component1;
 import com.aliucord.utils.ReflectUtils;
 import com.aliucord.Utils;
-import com.discord.models.domain.ModelMessageDelete;
+import com.discord.api.message.MessageTypes.*;
+import com.discord.stores.StoreMessageReplies.MessageState.*;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -84,14 +85,173 @@ class ReplyReferencesFix:Plugin(){
 			MessageEntry::class.java
 		)balls@{
 			frame ->
-			val messageEntry = frame.component1() as MessageEntry;
-			var type:Int? = null;
+			val messageEntry:MessageEntry = frame.component1();
+			var type:Int? = null as Int?;
 			//reflect
 			val replyHolder:View? = ReflectUtils.getField(this, "replyHolder");
 			val replyLinkItem:View? = ReflectUtils.getField(this, "replyLinkItem");
-			
+			val replyText:SimpleDraweeSpanTextView? = ReflectUtils.getField(this, "replyText");
+			val replyLeadingViewsHolder:View? = ReflectUtils.getField(this, "replyLeadingViewsHolder");
+			val adapter:WidgetChatListAdapter? = ReflectUtils.getField(this, "adapter");
+			fun configureReplyInteraction(messageEntry){
+				ReflectUtils.invokeMethod(
+					this,
+					"configureReplyInteraction",
+					arrayOf(messageEntry)
+				);
+			}
+			fun configureReplySystemMessage(resT, resS){
+				ReflectUtils.invokeMethod(
+					this,
+					"configureReplySystemMessage",
+					arrayOf(Utils.getResId(resT, resS))
+				);
+			}
+			fun configureReplySystemMessageUserJoin(messageEntry){
+				ReflectUtils.invokeMethod(
+					this,
+					"configureReplySystemMessageUserJoin",
+					arrayOf(messageEntry)
+				);
+			}
+			fun configureReplyAuthor(coreUser, user, messageEntry){
+				ReflectUtils.invokeMethod(
+					this,
+					"configureReplyAuthor",
+					arrayOf(coreUser, user, messageEntry)
+				);
+			}
+			fun getLeadingEdgeSpan(){
+				ReflectUtils.invokeMethod(
+					this,
+					"getLeadingEdgeSpan",
+					arrayOf<Any>()
+				);
+			}
+			fun configureReplyLayoutDirection(){
+				ReflectUtils.invokeMethod(
+					this,
+					"configureReplyLayoutDirection",
+					arrayOf<Any>()
+				);
+			}
+			fun configureReplyContentWithResourceId(resT, resS){
+				ReflectUtils.invokeMethod(
+					this,
+					"configureReplyContentWithResourceId",
+					arrayOf(Utils.getResId(resT, resS))
+				);
+			}
 			///reflect
 			if(replyHolder != null && replyLinkItem != null){
+				val message:Message = messageEntry.getMessage();
+				val replyData:MessageEntry.ReplyData = messageEntry.getReplyData();
+				val isInteraction:Boolean = message.isInteraction();
+				type = messageEntry.getMessage().getType();
+				if(isInteraction||(replyData != null && type == REPLY)){
+					replyHolder.setVisibility(View.VISIBLE);
+					replyLinkItem.setVisibility(View.VISIBLE);
+					if(isInteraction){
+						configureReplyInteraction(messageEntry);
+					}else if(replyData != null){
+						val messageEntry2:MessageEntry = replyData.getMessageEntry();
+						val messageState:StoreMessageReplies.MessageState =
+							replyData
+							.getMessageState()
+						;
+						if(replyData.isRepliedUserBlocked()){
+							configureReplySystemMessage(
+								"string", "reply_quote_message_blocked"
+							);
+						}else if(messageState is Unloaded){
+							configureReplySystemMessage(
+								"string", "reply_quote_message_not_loaded"
+							);
+						}else if(messageState is Deleted){
+							configureReplySystemMessage(
+								"string", "reply_quote_message_deleted"
+							);
+						}else if(messageState is Loaded && messageEntry2 != null){
+							val message2:Message = messageEntry2.getMessage();
+							replyHolder.setOnClickListener(
+								`WidgetChatListAdapterItemMessage$configureReplyPreview$1`(
+									message2
+								)
+							);
+							val type2:Int? = message2.getType();
+							if(type2 == USER_JOIN){
+								configureReplySystemMessageUserJoin(messageEntry2);
+								return@balls;
+							}
+							val author:User = message2.getAuthor();
+							configureReplyAuthor(CoreUser(author), author, messageEntry2);
+							if(replyText != null && replyLeadingViewsHolder != null){
+								val content:String? = message2.getContent();
+								if(content == null){
+									content = "";
+								}
+								if(!(content.length == 0)){
+									val context:Context = replyText.getContext();
+									val embeddedMessageParser:EmbeddedMessageParser =
+										EmbeddedMessageParser
+										.INSTANCE
+									;
+									val parse:DraweeSpanStringBuilder = embeddedMessageParser
+										.parse(
+											EmbeddedMessageParser.ParserData(
+												context,
+												messageEntry2.getRoles(),
+												messageEntry2.getNickOrUsernames(),
+												messageEntry2.getAnimateEmojis(),
+												StoreMessageState.State(
+													null,
+													null,
+													3,
+													null
+												),
+												50,
+												message2,
+												adapter
+											)
+										)
+									;
+									parse.setSpan(getLeadingEdgeSpan(), 0, parse.length, 33);
+									replyText?.setDraweeSpanStringBuilder(parse);
+									configureReplyLayoutDirection();
+								}else if(message2.hasStickers()){
+									configureReplyContentWithResourceId(
+										"string", "reply_quote_sticker_mobile"
+									);
+								}else if(
+									message2.hasAttachments()
+								||
+									message2.shouldShowReplyPreviewAsAttachment()
+								||
+									message2.hasEmbeds()
+								){
+									configureReplyContentWithResourceId(
+										"string", "reply_quote_no_text_content_mobile"
+									);
+								}else{
+									val appLog:AppLog = AppLog.g;
+									Logger.`e$default`(
+										appLog,
+										"Unhandled reply preview: "
+											+
+										messageEntry2,
+										null,
+										null,
+										6,
+										null
+									);
+								}
+							}
+						}
+					}
+				}else{
+					replyHolder?.setVisibility(View.GONE);
+					replyLinkItem?.setVisibility(View.GONE);
+				}
 			}
 		}
 	}
