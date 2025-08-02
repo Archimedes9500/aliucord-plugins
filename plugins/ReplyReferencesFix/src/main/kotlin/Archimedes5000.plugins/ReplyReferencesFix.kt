017298f872b2c239d8af398c24471aec7f89a433
Archimedes5000 <archimedes9500@gmail.com>
2025-08-02 02:26:17 +0200

    fix

--- plugins/ReplyReferencesFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
+++ plugins/ReplyReferencesFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
@@ -180,7 +180,7 @@ class ReplyReferencesFix:Plugin(){
 			val replyLinkItem = ReflectUtils.getField(this, "replyLinkItem") as View?;
 			val replyText = ReflectUtils.getField(this, "replyText") as SimpleDraweeSpanTextView?;
 			val replyLeadingViewsHolder = ReflectUtils.getField(this, "replyLeadingViewsHolder") as View?;
-			val adapter = `access$getAdapter$p`(this) as WidgetChatListAdapter;
+			val adapter:WidgetChatListAdapter = this.`access$getAdapter$p`(this);
 			fun configureReplyInteraction(messageEntry:MessageEntry){
 				ReflectUtils.invokeMethod(
 					this,
