63ffefb6f55c266eaf95d1ca5dd99ff859a0d411
GitHub Action <action@github.com>
2025-08-02 01:00:38 +0000

    Clone files from foreign repository

--- plugins/ReplyReferencesFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
+++ plugins/ReplyReferencesFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
@@ -180,7 +180,14 @@ class ReplyReferencesFix:Plugin(){
 			val replyLinkItem = ReflectUtils.getField(this, "replyLinkItem") as View?;
 			val replyText = ReflectUtils.getField(this, "replyText") as SimpleDraweeSpanTextView?;
 			val replyLeadingViewsHolder = ReflectUtils.getField(this, "replyLeadingViewsHolder") as View?;
-			val adapter:WidgetChatListAdapter = this.`access$getAdapter$p`(this);
+			val adapter = ReflectUtils
+				.invokeMethod(
+					this,
+					"access$getAdapter$p",
+					arrayOf(this)
+				)
+				as WidgetChatListAdapter?
+			;
 			fun configureReplyInteraction(messageEntry:MessageEntry){
 				ReflectUtils.invokeMethod(
 					this,
