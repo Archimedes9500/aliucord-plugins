b42bf4861593fcd944fb100b54357f1fcfd1965e
Archimedes5000 <archimedes9500@gmail.com>
2025-08-04 21:04:16 +0200

    fix

--- plugins/ReplyReferencesFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
+++ plugins/ReplyReferencesFix/src/main/kotlin/Archimedes5000.plugins/ReplyReferencesFix.kt
@@ -291,12 +291,6 @@ class ReplyReferencesFix:Plugin(){
 			///reflect
 			var type = null as Int?;
 			if(replyHolder != null && replyLinkItem != null){
-				val textRoot:ViewGroup = replyHolder.getParent() as ViewGroup;
-				//val clone = clone(replyHolder.getContext(), replyHolder);
-				//textRoot.addView(clone);
-				val test = TextView(textRoot.getContext());
-				test.text = "Test";
-				textRoot.addView(test);
 				val message:Message = messageEntry.getMessage();
 				val replyData:MessageEntry.ReplyData? = messageEntry.getReplyData();
 				val isInteraction:Boolean = message.isInteraction();
@@ -404,6 +398,12 @@ class ReplyReferencesFix:Plugin(){
 							}
 						}
 					}
+					val textRoot:ViewGroup = replyHolder.getParent() as ViewGroup;
+					//val clone = clone(replyHolder.getContext(), replyHolder);
+					//textRoot.addView(clone);
+					val test = TextView(textRoot.getContext());
+					test.text = "Test";
+					textRoot.addView(test);
 				}else{
 					replyHolder?.setVisibility(View.GONE);
 					replyLinkItem?.setVisibility(View.GONE);
