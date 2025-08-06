1699b40c94e6f901869a2f35aef64357c77d0ed6
GitHub Action <action@github.com>
2025-08-06 22:18:06 +0000

    Clone files from foreign repository

---
+++ plugin/ReplyReferencesFix/src/main/kotlin/Archimedes5000.plugins/util/ReflectUtils.java
@@ -0,0 +1,29 @@
+package Archimedes5000.plugins.util;
+import java.lang.reflect.*;
+class ReflectUtils{
+	public static ExistingProp propRef(Class<?> c, String name, Class<?>... classes){
+		Object prop;
+		if(classes.length == 0){
+			try{
+				prop = c.getDeclaredField(name);
+				return new ExistingProp((Field)prop);
+			}catch(NoSuchFieldException e){
+				try{
+					prop = c.getDeclaredMethod(name);
+					return new ExistingProp((Method)prop);
+				}catch(NoSuchMethodException ee){
+					ee.printStackTrace();
+					return null;
+				}
+			}
+		}else{
+			try{
+				prop = c.getDeclaredMethod(name, classes);
+				return new ExistingProp((Method)prop);
+			}catch(NoSuchMethodException e){
+				e.printStackTrace();
+				return null;
+			}
+		}
+	}
+}
