89f2f601f36649d19bf975c400abac448c3b910c
GitHub Action <action@github.com>
2025-08-30 18:43:49 +0000

    Clone files from foreign repository

--- plugin/BetterReplaceText/src/main/java/alt.archimedes5000.plugins/BetterReplaceText.java
+++ plugin/BetterReplaceText/src/main/java/alt.archimedes5000.plugins/BetterReplaceText.java
@@ -3,12 +3,26 @@ import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.entities.Plugin;
 import android.annotation.SuppressLint;
 import android.content.Context;
-import com.aliucord.patcher.patch;
 import com.aliucord.patcher.PreHook;
 import com.discord.api.message.Message;
+import com.aliucord.wrappers.MessageWrapper;
 //import com.discord.models.message.Message as MessageModel;
 import com.discord.stores.StoreMessages;
 import com.aliucord.Logger;
+
+import com.discord.api.application.Application;
+import com.discord.api.botuikit.Component;
+import com.discord.api.channel.Channel;
+import com.discord.api.guildmember.GuildMember;
+import com.discord.api.interaction.Interaction;
+import com.discord.api.message.activity.MessageActivity;
+import com.discord.api.message.attachment.MessageAttachment;
+import com.discord.api.message.call.MessageCall;
+import com.discord.api.message.embed.MessageEmbed;
+import com.discord.api.message.reaction.MessageReaction;
+import com.discord.api.user.User;
+import com.discord.api.utcdatetime.UtcDateTime;
+import java.util.List;
 
 @AliucordPlugin(requiresRestart = false)
 public class BetterReplaceText extends Plugin {
@@ -56,8 +70,8 @@ public class BetterReplaceText extends Plugin {
 			new PreHook(frame -> {
 				Message message = (Message)frame.thisObject;
 				//this.content = Utils.replace(frame.args[3]);
-				message.content = frame.args[3]+"aaa";
-				Logger().debug(message.toString);
+				MessageWrapper(message).getContent = frame.args[3]+"aaa";
+				new Logger().debug(message.toString());
 			})
 		);
 	}
