8ecb1a9172c304d7f64d2d77185f1d2ef96e61c4
GitHub Action <action@github.com>
2025-09-02 21:31:59 +0000

    Clone files from foreign repository

--- plugin/BetterReplaceText/src/main/java/alt.archimedes5000.plugins/BetterReplaceText.java
+++ plugin/BetterReplaceText/src/main/java/alt.archimedes5000.plugins/BetterReplaceText.java
@@ -20,66 +20,20 @@ public class BetterReplaceText extends Plugin {
     @Override
 	public void start(Context pluginContext) throws Throwable{
 		patcher.patch(
-			EmbeddedMessageParser.class
+			DiscordParser.class
 			.getDeclaredMethod(
-				"parse",
-				ParserData.class
+				"parseChannelMessage",
+				Context,
+				String,
+				MessageRenderContext,
+				MessagePreprocessor,
+				ParserOptions,
+				boolean
 			),
 			new PreHook(frame -> {
-				EmbeddedMessageParser parser = (EmbeddedMessageParser)frame.thisObject;
-				ParserData data = (ParserData)frame.args[0];
-				Message message = data.getMessage();
-				frame.args[0] = new ParserData(
-					data.component1(),
-					data.component2(),
-					data.component3(),
-					data.component4(),
-					data.component5(),
-					data.component6(),
-					new Message(
-						message.component1(),
-						message.component2(),
-						message.component3(),
-						message.component4(),
-						"https://youtu.be/7MHYrDk2rG0",
-						message.component6(),
-						message.component7(),
-						message.component8(),
-						message.component9(),
-						message.component10(),
-						message.component11(),
-						message.component12(),
-						message.component13(),
-						message.component14(),
-						message.component15(),
-						message.component16(),
-						message.component17(),
-						message.component18(),
-						message.component19(),
-						message.component20(),
-						message.component21(),
-						message.component22(),
-						message.component23(),
-						message.component24(),
-						message.component25(),
-						message.component26(),
-						message.component27(),
-						message.component28(),
-						message.component29(),
-						message.component30(),
-						message.component31(),
-						message.component32(),
-						message.component33(),
-						message.component34(),
-						message.component35(),
-						message.component36(),
-						message.component37(),
-						message.component38(),
-						message.component39()
-					),
-					data.component8()
-				);
-				new Logger().debug(frame.args[0].toString());
+				DiscordParser parser = (DiscordParser)frame.thisObject;
+				frame.args[1] = "https://youtu.be/7MHYrDk2rG0";
+				new Logger().debug(frame.thisObject.toString());
 			})
 		);
 	}
