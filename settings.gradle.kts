d9c076c910c9b8073f94a71393431a6a1f7d5bfe
Archimedes5000 <archimedes9500@gmail.com>
2025-11-17 23:17:47 +0100

    fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,29 +1,30 @@
 @file:Suppress("UnstableApiUsage")
 
 pluginManagement {
-    repositories {
-        google()
-        gradlePluginPortal()
-        maven("https://maven.aliucord.com/snapshots")
-        maven("https://jitpack.io")
-    }
+	repositories {
+		google()
+		gradlePluginPortal()
+		maven("https://maven.aliucord.com/snapshots")
+		maven("https://maven.aliucord.com/releases")
+		maven("https://jitpack.io")
+	}
 }
 
 dependencyResolutionManagement {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://maven.aliucord.com/snapshots")
-    }
+	repositories {
+		google()
+		mavenCentral()
+		maven("https://maven.aliucord.com/snapshots")
+	}
 
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+	repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 }
 
 rootProject.name = "aliucord-plugins"
 
 rootDir
-    .resolve("plugin")
-    .listFiles { file ->
-        file.isDirectory && file.resolve("build.gradle.kts").exists()
-    }!!
-    .forEach { include(":plugin:${it.name}") }
+	.resolve("plugin")
+	.listFiles { file ->
+		file.isDirectory && file.resolve("build.gradle.kts").exists()
+	}!!
+	.forEach { include(":plugin:${it.name}") }
