758f7dcf1cc1b911162a2416cfe1346bfb95430f
GitHub Action <action@github.com>
2025-11-20 02:37:41 +0000

    Clone files from foreign repository

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -7,63 +7,64 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidExtension
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
 
 subprojects {
-    val libs = rootProject.libs
+	val libs = rootProject.libs
 
-    apply {
-        plugin(libs.plugins.android.library.get().pluginId)
-        plugin(libs.plugins.aliucord.plugin.get().pluginId)
-        plugin(libs.plugins.kotlin.android.get().pluginId)
-        plugin(libs.plugins.ktlint.get().pluginId)
-    }
+	apply {
+		plugin(libs.plugins.android.library.get().pluginId)
+		plugin(libs.plugins.aliucord.plugin.get().pluginId)
+		plugin(libs.plugins.kotlin.android.get().pluginId)
+		plugin(libs.plugins.ktlint.get().pluginId)
+	}
 
-    configure<LibraryExtension> {
-        namespace = "alt.archimedes5000.plugins"
-        compileSdk = 36
+	configure<LibraryExtension> {
+		namespace = "alt.archimedes5000.plugins"
+		compileSdk = 36
 
-        defaultConfig {
-            minSdk = 21
-        }
+		defaultConfig {
+			minSdk = 21
+		}
 
-        buildFeatures {
-            aidl = false
-            buildConfig = true
-            renderScript = false
-            shaders = false
-        }
+		buildFeatures {
+			aidl = false
+			buildConfig = true
+			renderScript = false
+			shaders = false
+		}
 
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_21
-            targetCompatibility = JavaVersion.VERSION_21
-        }
-    }
+		compileOptions {
+			sourceCompatibility = JavaVersion.VERSION_21
+			targetCompatibility = JavaVersion.VERSION_21
+		}
+	}
 
-    configure<AliucordExtension> {
-        author("Archimedes5000", 0L)
-        github("https://github.com/Archimedes9500/aliucord-plugins/")
-    }
+	configure<AliucordExtension> {
+		author("Archimedes5000", 0L)
+		github("https://github.com/Archimedes9500/aliucord-plugins/")
+	}
 
-    configure<KtlintExtension> {
-        version.set(libs.versions.ktlint.asProvider())
+	configure<KtlintExtension> {
+		version.set(libs.versions.ktlint.asProvider())
 
-        coloredOutput.set(true)
-        outputColorName.set("RED")
-        ignoreFailures.set(true)
-    }
+		coloredOutput.set(true)
+		outputColorName.set("RED")
+		ignoreFailures.set(true)
+	}
 
-    configure<KotlinAndroidExtension> {
-        compilerOptions {
-            jvmTarget = JvmTarget.JVM_21
-            optIn.add("kotlin.RequiresOptIn")
-        }
-    }
+	configure<KotlinAndroidExtension> {
+		compilerOptions {
+			jvmTarget = JvmTarget.JVM_21
+			optIn.add("kotlin.RequiresOptIn")
+		}
+	}
 
-    @Suppress("unused")
-    dependencies {
-        val compileOnly by configurations
-        val implementation by configurations
+	@Suppress("unused")
+	dependencies {
+		val compileOnly by configurations
+		val implementation by configurations
 
-        compileOnly(libs.discord)
-        compileOnly(libs.aliucord)
-        compileOnly(libs.kotlin.stdlib)
-    }
+		compileOnly(libs.discord)
+		compileOnly(libs.aliucord)
+		compileOnly(libs.kotlin.stdlib)
+		compileOnly("com.aliucord:Aliuhook:1.1.4")
+	}
 }
