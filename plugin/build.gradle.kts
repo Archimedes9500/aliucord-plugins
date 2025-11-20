b6def593364d1806c8c5effa6ca529ee1a3afa8f
GitHub Action <action@github.com>
2025-11-20 01:11:17 +0000

    Clone files from foreign repository

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,73 +2,68 @@
 
 import com.aliucord.gradle.AliucordExtension
 import com.android.build.gradle.LibraryExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidExtension
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
-
-val repo = "Archimedes9500/aliucord-plugins"
 
 subprojects {
     val libs = rootProject.libs
 
     apply {
         plugin(libs.plugins.android.library.get().pluginId)
-        plugin(libs.plugins.aliucord.get().pluginId)
+        plugin(libs.plugins.aliucord.plugin.get().pluginId)
         plugin(libs.plugins.kotlin.android.get().pluginId)
         plugin(libs.plugins.ktlint.get().pluginId)
     }
 
     configure<LibraryExtension> {
         namespace = "alt.archimedes5000.plugins"
-
-        compileSdk = 34
+        compileSdk = 36
 
         defaultConfig {
-            minSdk = 24
+            minSdk = 21
         }
 
         buildFeatures {
+            aidl = false
+            buildConfig = true
             renderScript = false
             shaders = false
-            buildConfig = true
-            resValues = false
-            aidl = false
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
         }
     }
 
     configure<AliucordExtension> {
-        author("Archimedes5000", 0L)
-
-        updateUrl.set("https://raw.githubusercontent.com/$repo/builds/updater.json")
-        buildUrl.set("https://raw.githubusercontent.com/$repo/builds/%s.zip")
+        author("Archumedes5000", 0L)
+        github("https://github.com/Archimedes9500/aliucord-plugins/")
     }
 
     configure<KtlintExtension> {
-        version.set(libs.versions.ktlint)
+        version.set(libs.versions.ktlint.asProvider())
 
         coloredOutput.set(true)
         outputColorName.set("RED")
         ignoreFailures.set(true)
     }
 
+    configure<KotlinAndroidExtension> {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
+            optIn.add("kotlin.RequiresOptIn")
+        }
+    }
+
+    @Suppress("unused")
     dependencies {
-        val discord by configurations
         val compileOnly by configurations
         val implementation by configurations
 
-        discord(libs.discord)
+        compileOnly(libs.discord)
         compileOnly(libs.aliucord)
-        //compileOnly("com.github.Aliucord:Aliucord:unspecified")
-    }
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "11"
-            freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
-        }
+        compileOnly(libs.kotlin.stdlib)
     }
 }
