e259dec8df726c210a90de5fe6fe93b63afe7829
GitHub Action <action@github.com>
2025-11-20 19:33:18 +0000

    Clone files from foreign repository

--- plugin/NCP/src/main/kotlin/alt._7hereape.plugins/NCP.kt
+++ plugin/NCP/src/main/kotlin/alt._7hereape.plugins/NCP.kt
@@ -15,42 +15,8 @@ import com.discord.models.message.Message
 
 @AliucordPlugin
 class NCP: Plugin(){
-	val ver = "nana";
-	val key: HashMap<String, Array<Int>> = hashMapOf(
-		"nana" to arrayOf()
-	);
 	fun encrypt(string: String): String{
-		var s = StringBuilder(string);
-		val r = Regex("""<.*?>|[\[\]\(\)*~#-_>\n]""");
-		val matches = r.findAll(string, 0);
-	
-		var out = StringBuilder(string);
-		if(key[ver] != null){
-			for(i in out.indices){
-				if(matches.any{m -> i in m.range}) continue;
-				//out[i] = (s[i]?.toInt()!!+key[ver]?.get(i)!!)?.toChar()!!;
-			};
-		}else{
-			return string;
-		};
-		return ver+out.toString();
-	};
-	fun decrypt(string: String): String{
-		val v = string.substring(0, 4)
-		var s = StringBuilder(string.substring(4));
-		val r = Regex("""<.*?>|[\[\]\(\)*~#-_>\n]""");
-		val matches = r.findAll(string, 4);
-	
-		var out = StringBuilder(string);
-		if(key[ver] != null){
-			for(i in out.indices){
-				if(matches.any{m -> i in m.range}) continue;
-				//out[i] = (s[i]?.toInt()!!-key[ver]?.get(i)!!)?.toChar()!!;
-			};
-		}else{
-			return string;
-		};
-		return out.toString();
+		return string;
 	};
 
 	override fun start(pluginContext: Context){
