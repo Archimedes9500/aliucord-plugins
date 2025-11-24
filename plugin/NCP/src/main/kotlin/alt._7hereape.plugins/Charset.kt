0e77563497e4d1324f4d2c94e158773f3b249b37
GitHub Action <action@github.com>
2025-11-24 18:51:34 +0000

    Clone files from foreign repository

--- plugin/NCP/src/main/kotlin/alt._7hereape.plugins/Charset.kt
+++ plugin/NCP/src/main/kotlin/alt._7hereape.plugins/Charset.kt
@@ -2,14 +2,14 @@ package alt._7hereape.plugins
 
 class Charset(vararg ranges: IntRange){
 	val list: List<Int> = ranges.flatMap{it.toList()}.distinct().sorted();
-	val min = list.first;
-	val max = list.last;
+	val min = list.first();
+	val max = list.last();
 
 	operator fun get(i: Int): Int{
 		return list[mod((i-min), (max-min+1))+min]!!;
 	};
 	fun char(string: StringBuilder, index: Int, offset: Int): Char{
-		val codepoint = string[i]!!.toInt();
+		val codepoint = string[index]!!.toInt();
 		return this[codepoint+offset].toChar();
 	};
 };
