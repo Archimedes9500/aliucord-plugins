75e75ca147bc7273a721a155f67d4d3ac3d287bf
Archimedes5000 <archimedes9500@gmail.com>
2025-11-27 14:06:37 +0100

    fix

--- plugin/NCP/src/main/kotlin/alt._7hereape.plugins/Charset.kt
+++ plugin/NCP/src/main/kotlin/alt._7hereape.plugins/Charset.kt
@@ -1,7 +1,5 @@
 package alt._7hereape.plugins
 
-import com.aliucord.Logger
-
 class Charset(vararg ranges: IntRange){
 	val list: List<Int> = ranges.flatMap{it.toList()}.distinct().sorted();
 	val min = 0;
@@ -12,7 +10,6 @@ class Charset(vararg ranges: IntRange){
 	};
 	fun char(string: StringBuilder, index: Int, offset: Int): Char{
 		val codepoint = string[index]!!.toInt();
-		Logger().debug(codepoint.toString()+" + "+offset.toString()+" = "+(codepoint+offset).toString()+" -> "+this[codepoint+offset]+" => "+this[codepoint+offset].toChar());
 		return this[codepoint+offset].toChar();
 	};
 };
