94453673084dd764ab32e5353c16cfdb229898aa
Archimedes5000 <archimedes9500@gmail.com>
2026-01-08 19:30:45 +0100

    fix

---
+++ plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
@@ -0,0 +1,26 @@
+package alt.archimedes5000.plugins
+import com.aliucord.annotations.AliucordPlugin
+import android.annotation.SuppressLint
+import com.aliucord.entities.Plugin
+import android.content.Context
+import com.aliucord.Utils.showToast
+import com.aliucord.SettingsUtilsJSON
+import com.discord.stores.StoreStream
+
+@AliucordPlugin(requiresRestart = false)
+class JSOPTest: Plugin(){
+	@SuppressLint("SetTextI18n")
+	val settings = SettingsUtilsJSON("JSOPTest");
+	override fun start(pluginContext: Context){
+		val imports = settings.getObject("import", Map<String, String>::class);
+		val jsop = JSOP(imports, object{
+			val `$author`: User = StoreStream.getUsers().me;
+		});
+
+		val body = settings.getObject("body", JSONArray::class);
+		if(jsop.run<Boolean?>(body.getJSONArray(0))){
+			showToast("Yuore not a bot", showLonger = true);
+		};
+	};
+	override fun stop(pluginContext: Context) = patcher.unpatchAll();
+};
--- plugin/NCP/src/main/kotlin/alt._7hereape.plugins/Charset.kt
+++ plugin/NCP/src/main/kotlin/alt._7hereape.plugins/Charset.kt
@@ -2,18 +2,22 @@ package alt._7hereape.plugins
 
 import com.aliucord.Logger
 
-class Charset(vararg ranges: IntRange){
-	val list: List<Int> = ranges.flatMap{it.toList()}.distinct().sorted();
+class Charset(vararg ranges: CharRange){
+	val list: List<Char> = ranges.flatMap{it.toList()}.distinct().sorted();
 	val min = 0;
 	val max = list.lastIndex;
 
-	operator fun get(i: Int): Int{
+	operator fun get(i: Int): Char{
+		if(i in min..max) return list[i]!!;
 		return list[mod((i-min), (max-min+1))+min]!!;
 	};
 	fun char(string: StringBuilder, index: Int, offset: Int): Char{
 		val codepoint = string[index]!!.toInt();
-		Logger().debug(codepoint.toString()+" + "+offset.toString()+" = "+(codepoint+offset).toString()+" -> "+this[codepoint+offset].toString()+" => "+this[codepoint+offset].toChar());
 		return this[codepoint+offset].toChar();
+	};
+	fun index(string: StringBuilder, index: Int, offset: Int): Int{
+		val char = string[index]!!;
+		return this.indexOf(char)-offset;
 	};
 };
 fun mod(x: Int, y: Int): Int{
