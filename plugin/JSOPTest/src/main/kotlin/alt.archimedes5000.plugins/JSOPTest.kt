cd5b066e68f13c5b725539b07a4f9343d8c6b34c
GitHub Action <action@github.com>
2026-01-09 14:14:50 +0000

    Clone files from foreign repository

--- plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
+++ plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
@@ -12,18 +12,18 @@ import org.json.*
 @AliucordPlugin(requiresRestart = true)
 class JSOPTest: Plugin(){
 	@SuppressLint("SetTextI18n")
-	val settings2 = SettingsUtilsJSON("JSOPTest");
 	override fun start(pluginContext: Context){
-		val imports = settings2.getObject<Map<String, String>>("import", mapOf());
+		val imports = settings.getObject<Map<String, String>>("import", mapOf());
 		val jsop = JSOP(imports, object{
 			val `$me`: MeUser = StoreStream.getUsers().me;
 		});
 
 		val body = settings.getObject<JSONArray>("balls", JSONArray());
-		if(jsop.run<Boolean>(body.getJSONArray(0)) == true){
-			showToast("Yuore a bot", showLonger = true);
-		}else{
-			showToast("Yuore not a bot", showLonger = true);
+		val isBot = jsop.run<Boolean>(body.optJSONArray(0));
+		when(isBot){
+			false -> showToast("Yuore not a bot", showLonger = true);
+			true-> showToast("Yuore a bot", showLonger = true);
+			else -> showToast("Idk wtf yuore", showLonger = true);
 		};
 	};
 	override fun stop(pluginContext: Context) = patcher.unpatchAll();
