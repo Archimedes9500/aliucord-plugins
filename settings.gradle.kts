d77896902ddc3355b462c2fd3f7755ec2b97def1
Archimedes9500 <82618548+Archimedes9500@users.noreply.github.com>
2026-01-10 23:00:07 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ dependencyResolutionManagement {
         mavenCentral()
         maven {
             name = "aliucord"
-            url = uri("https://maven.aliucord.com/releases")
+            url = uri("https://maven.aliucord.com/snapshots")
         }
     }
 }
