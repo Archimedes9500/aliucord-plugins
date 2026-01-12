10eec16ec2c7abd4d611fc652679b58f872f51b5
Archimedes5000 <archimedes9500@gmail.com>
2026-01-12 10:26:52 +0100

    fix

--- plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOP.kt
+++ plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOP.kt
@@ -14,12 +14,66 @@ class JSOP(
 		val args: List<Pair<String, Any?>>
 	);
 
+	//error utils
+	var line: Int = 0;
+	lateinit lastExpr: JSONArray;
+	val errors = ArrayList<String>();
+
+	//errors
+	fun INVALID_ARGUMENT(arg: JSONObject, reason: String): String{
+		return """
+			ERROR invalid argument: $arg
+				because it $reason
+				on line ${this.line}:
+				${this.lastExpr}
+		""".trimIndent();
+	};
+	fun INVALID_VALUE(value: Any?, reason: String): String{
+		return """
+			ERROR invalid value: $value
+				because it $reason
+				on line ${this.line}:
+				${this.lastExpr}
+		""".trimIndent();
+	};
+	fun LINE_SKIPPED(value: Any?): String{
+		return """
+			ERROR skipped line: ${this.line}
+				because it was invalid.
+		""".trimIndent();
+	};
+	fun CONVERSION_FAILED(type: String, value: Any?): String{
+		return """
+			ERROR failed to convert value: $value
+				to type: $type
+				on line ${this.line}:
+				${this.lastExpr}
+		""".trimIndent();
+	};
+	fun UNKNOWN_TYPE(arg: Pair<String, Any?>): String{
+		return """
+			ERROR unknown type: $type
+				(it was not declared in "imports")
+				on line ${this.line}:
+				${this.lastExpr}
+		""".trimIndent();
+	};
+
+
 	//parse utils
 	fun parseArg(obj: JSONObject): Pair<String, Any?>?{
 		val keys = obj.keys();
-		if(!keys.hasNext()) return null;
+		if(!keys.hasNext()){
+			errors.add(INVALID_ARGUMENT(obj, "has no keys"));
+			return null;
+		};
+
 		val key = keys.next();
-		if(keys.hasNext()) return null;
+		if(keys.hasNext()){
+			errors.add(INVALID_ARGUMENT(obj, "has more than 1 key"));
+			return null;
+		};
+
 		val rawValue = obj.get(key);
 		when(rawValue){
 			is JSONArray -> {
@@ -27,6 +81,7 @@ class JSOP(
 				if(value!= null){
 					return Pair(key, value);
 				}else{
+					errors.add(INVALID_VALUE(obj, "is not a valid Expression or Array of Args"));
 					return null;
 				};
 			};
@@ -35,6 +90,7 @@ class JSOP(
 				if(value!= null){
 					return Pair(key, value);
 				}else{
+					errors.add(INVALID_VALUE(obj, "is not a valid Object of Args"));
 					return null;
 				};
 			};
@@ -43,29 +99,48 @@ class JSOP(
 		};
 	};
 	fun parseExpr(expr: JSONArray): Expression?{
-		val name = expr.optString(0)?: return null;
-		val reciever = parseArg(expr.optJSONObject(1)?: return null);
-		if(reciever == null) return null;
+		val name = expr.optString(0);
+		if(name == null){
+			errors.add(INVALID_VALUE(expr, "is not a valid Expression, it has no name"))
+			return null;
+		};
+
+		val rawReciever = expr.optJSONObject(1);
+		if(rawReciever == null){
+			errors.add(INVALID_VALUE(expr.opt(1), "is not an Object or doesn't exist"));
+			return null;
+		};
+		val reciever = parseArg(rawReciever)?: return null;
+
 		val args = ArrayList<Pair<String, Any?>>();
 		for(i in 2 until expr.length()){
-			val arg = parseArg(expr.optJSONObject(i)?: return null);
-			if(arg == null) return null;
+			val rawArg = expr.optJSONObject(i);
+			if(rawArg == null){
+				errors.add(INVALID_VALUE(expr.opt(i), "is not an Object"));
+			};
+			val arg = parseArg(rawArg)?: return null;
 			args.add(arg);
 		};
+
 		return Expression(name!!, reciever!!, args!!);
 	}
 	fun parseArray(array: JSONArray): ArrayList<Pair<String, Any?>>?{
 		val types = ArrayList<String>();
 		val output = ArrayList<Pair<String, Any?>>();
 		for(i in 0 until array.length()){
-			val e = parseArg(array.optJSONObject(i)?: return null);
-			if(e == null) return null;
+			val rawE = array.optJSONObject(i);
+			if(rawE == null){
+				errors.add(INVALID_VALUE(array.opt(i), "is not an Object"));
+			};
+			val e = parseArg(rawE)?: return null;
 			types.add(e.first);
 			output.add(e);
 		};
+
 		if(types.distinct().size <= 1){
 			return output
 		}else{
+			errors.add(INVALID_VALUE(array, "is not a valid Array of Args with uniform type"));
 			return null;
 		};
 	};
@@ -75,27 +150,37 @@ class JSOP(
 		val keys = obj.keys();
 		while(keys.hasNext()){
 			val key = keys.next();
-			val arg = parseArg(obj.optJSONObject(key)?: return null);
-			if(arg == null) return null;
+			val rawArg = obj.optJSONObject(key);
+			if(rawArg == null){
+				errors.add(INVALID_VALUE(obj.opt(key), "is not an Object"));
+			};
+			val arg = parseArg(rawArg)?: return null;
 			types.add(arg.first);
 			output.set(key, arg);
 		};
+
 		if(types.distinct().size <= 1){
 			return output
 		}else{
+			errors.add(INVALID_VALUE(array, "is not a valid Object of Args with uniform type"));
 			return null;
 		};
 	};
-	fun parse(body: JSONArray): ArrayList<Expression>{
+	/*
+	fun parse(body: JSONArray): ArrayList<Expression?>{
 		val output = ArrayList<Expression>();
 		for(i in 0 until body.length()){
+			this.line++;
 			val expr = parseExpr(body.getJSONArray(i));
-			if(expr != null){
-				output.add(expr);
+			if(expr == null){
+				errors.add(LINE_SKIPPED());
 			};
+			output.add(expr);
 		};
+
 		return output;
 	};
+	*/
 
 	//exec utils
 	fun envSetup(value: Any?): Any?{
@@ -104,6 +189,7 @@ class JSOP(
 				.getDeclaredField(value.toString())
 				.apply{isAccessible = true}
 			;
+
 			return field.get(this.env);
 		}catch(e: NoSuchFieldException){
 			return value;
@@ -111,22 +197,33 @@ class JSOP(
 	};
 	fun convert(arg: Pair<String, Any?>): Pair<String, Any?>?{
 		val (type, value) = arg;
-		if(value == null) return null;
+		if(value == null) return arg;
+
 		val o = value::class.java.declaredMethods
-			?.find{it.name == "to"+type}
+			?.find{
+				it.name == "to"+type;
+				it.parameterCount = 0;
+			}
 			?.apply{isAccessible = true}
 			?.invoke(value)
 		;
+
 		if(o != null && o::class == value::class){
 			return Pair(type, o);
 		}else{
+			errors.add(CONVERSION_FAILED(type, value));
 			return null;
 		};
 	};
 	fun processArg(arg: Pair<String, Any?>): Pair<String, Any?>?{
 		var (type, value) = arg;
+
 		val setUpValue = envSetup(value);
-		val argClass = Class.forName(imports[type]!!);
+		val argClass = Class.forName(imports[type]);
+		if(argaClass == null){
+			errors.add(UNKNOWN_TYPE(type));
+		};
+
 		return if(value == null){
 			arg;
 		}else if(setUpValue == null){
@@ -140,38 +237,74 @@ class JSOP(
 		};
 	};
 	fun exec(returnType: String, expr: Expression): Any?{
-		val name = expr.name;
+		this.lastExpr = expr;
 
+		val name = expr.name;
 		val reciever = processArg(expr.reciever);
 		val recieverType = expr.reciever.first;
-		val args = expr.args.map{processArg(it)?.second}.toTypedArray();
+
+		val types = expr.args
+			.map{
+				Class.forName(
+					imports[
+						processArg(it)?.first
+					]
+				)
+			}
+			.toTypedArray()
+		;
+		val args = expr.args
+			.map{
+				processArg(it)?.second
+			}
+			.toTypedArray()
+		;
 
 		val returnClass = if(returnType != ""){
-			Class.forName(imports[returnType]!!);
+			Class.forName(imports[returnType]);
 		}else{
 			defaultClass;
 		};
-		val recieverClass = Class.forName(imports[recieverType]!!);
+		if(returnClass == null){
+			errors.add(UNKNOWN_TYPE(returnType));
+			return null;
+		};
+
+		val recieverClass = Class.forName(imports[recieverType]);
+		if(recieverClass == null){
+			errors.add(UNKNOWN_TYPE(recieverClass));
+			return null;
+		};
 
 		var returnValue: Any? = null;
-		val method = recieverClass.declaredMethods
-			?.find{it.name == name}
-			?.apply{isAccessible = true}
-		;
+		
 		//try method
-		if(method != null){
+		try{
+			val method = recieverClass.getDeclaredMethod(name, *types)?.apply{isAccessible = true};
 			returnValue = method.invoke(reciever?.second, *args);
-		}else if(args.size == 0){
-			val field = recieverClass.declaredFields
-				?.find{it.name == name}
-				?.apply{isAccessible = true}
-			;
-			//try field
-			if(field != null){
-				returnValue = field.get(reciever?.second);
+		}catch(e: NoSuchMethodException){
+			//try constructor
+			try{
+				val thisClass = Class.forName(imports[name]);
+				if(thisClass == null) throw NoSuchMethodException();
+				//expr name resolves to a class
+				val constr = thisClass.getDeclaredConstructor(*types)?.apply{isAccessible = true};
+				returnValue = constr.invoke(*args);
+			}catch(e: NoSuchMethodException){
+				//try field
+				if(args.size == 0){
+					try{
+						val field = recieverClass.declaredField(name)?.apply{isAccessible = true};
+						returnValue = field.get(reciever?.second);
+					}catch(e: NoSuchFieldException){
+						returnValue = null;
+					};
+				};
 			};
 		};
+
 		if(returnValue == null){
+			errors.add(INVALID_VALUE(expr, "is not a valid Expression, name did not resolve to method, constructor or field"));
 			return returnValue;
 		}else if(returnValue::class.java == returnClass){
 			return returnValue;
@@ -181,20 +314,28 @@ class JSOP(
 	};
 
 	//public interface
-	inline fun <reified T>run(line: JSONArray): T?{
+	inline fun <reified T>run(line: JSONArray): Pair<T?, String?>?{
+		this.line++;
 		val expr = parseExpr(line);
 		if(expr != null){
 			this.defaultClass = T::class.java;
-			return exec("", expr) as? T;
+			return Pair(
+				exec("", expr) as? T,
+				this.errors[this.errors.size]
+			);
 		}else{
 			return null;
 		};
 	};
-	inline fun <reified T>run(body: JSONArray, results: ArrayList<T>){
-		val exprs = parse(body);
-		this.defaultClass = T::class.java;
-		for(expr in exprs){
-			results.add(exec("", expr) as T);
+	inline fun <reified T>run(body: JSONArray, results: ArrayList<T?>): ArrayList<String>{
+		for(line in body){
+			this.line++
+			val expr = parseExpr(line);
+			if(expr != null){
+				this.defaultClass = T::class.java;
+				results.add(exec("", expr) as? T);
+			};
 		};
+		return this.errors;
 	};
 };
