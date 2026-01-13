58e48d8cbbbe94ff02909455a2ba558bd71adb64
GitHub Action <action@github.com>
2026-01-13 12:49:27 +0000

    Clone files from foreign repository

--- plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
+++ plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
@@ -22,8 +22,23 @@ class JSOPTest: Plugin(){
 	@SuppressLint("SetTextI18n")
 	val settings2 = SettingsUtilsJSON("JSOPTest");
 
-	override fun start(pluginContext: Context){
+	override fun start(pluginContext: Context){/*
+		val imports = settings.getObject<MutableMap<String, String>>("imports", mutableMapOf());
+		val jsop = JSOP(imports, object{
+			val `$me`: MeUser = StoreStream.getUsers().me;
+		});
+
+		val body = settings2.getJSONArray("body", JSONArray())!!;
+		val (isBot, errors) = jsop.run<Boolean>(body.getJSONArray(0));
+		if(!errors.isEmpty()) logger.debug(errors.joinToString("\n"));
+		when(isBot){
+			false -> showToast("Yuore not a bot", showLonger = true);
+			true-> showToast("Yuore a bot", showLonger = true);
+			else -> showToast("Idk wtf yuore", showLonger = true);
+		};
+*/
 		val viewID = Utils.getResId("chat_list_adapter_item_text", "id");
+		logger.debug("start: "+viewID);
 		patcher.patch(
 			WidgetChatListAdapterItemTextBinding::class.java
 				.getDeclaredMethod("a", View::class.java)
@@ -36,7 +51,15 @@ class JSOPTest: Plugin(){
 				if(id != viewID){
 					logger.debug("failed: "+id+" ≠ "+viewID);
 					return@Hook;
-				};
+				};/*
+				view.setBackgroundColor(4278190080L
+					.or(Instant.now()
+						.toEpochMilli()
+					).rem(16777215)
+					.toInt()
+				);
+				frame.setResult(result);
+*/
 				logger.debug("success "+view.toString());
 			}
 		);
