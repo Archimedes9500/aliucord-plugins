02473fae7d7f1ca6d962dcf035f11cc1075a31d8
Archimedes5000 <archimedes9500@gmail.com>
2026-01-13 13:39:09 +0100

    fix

--- plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
+++ plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
@@ -22,21 +22,7 @@ class JSOPTest: Plugin(){
 	@SuppressLint("SetTextI18n")
 	val settings2 = SettingsUtilsJSON("JSOPTest");
 
-	override fun start(pluginContext: Context){/*
-		val imports = settings.getObject<MutableMap<String, String>>("imports", mutableMapOf());
-		val jsop = JSOP(imports, object{
-			val `$me`: MeUser = StoreStream.getUsers().me;
-		});
-
-		val body = settings2.getJSONArray("body", JSONArray())!!;
-		val (isBot, errors) = jsop.run<Boolean>(body.getJSONArray(0));
-		if(!errors.isEmpty()) logger.debug(errors.joinToString("\n"));
-		when(isBot){
-			false -> showToast("Yuore not a bot", showLonger = true);
-			true-> showToast("Yuore a bot", showLonger = true);
-			else -> showToast("Idk wtf yuore", showLonger = true);
-		};
-*/
+	override fun start(pluginContext: Context){
 		val viewID = Utils.getResId("chat_list_adapter_item_text", "id");
 		patcher.patch(
 			WidgetChatListAdapterItemTextBinding::class.java
@@ -51,13 +37,6 @@ class JSOPTest: Plugin(){
 					logger.debug("failed: "+id+" ≠ "+viewID);
 					return@Hook;
 				};
-				view.setBackgroundColor(4278190080L
-					.or(Instant.now()
-						.toEpochMilli()
-					).rem(16777215)
-					.toInt()
-				);
-				frame.setResult(result);
 				logger.debug("success "+view.toString());
 			}
 		);
