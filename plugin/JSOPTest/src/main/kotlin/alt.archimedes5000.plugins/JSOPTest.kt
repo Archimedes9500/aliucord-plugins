5ad0d0d5d7d7ea32e730d2fe50a7bd5d51dc42df
GitHub Action <action@github.com>
2026-01-13 09:00:09 +0000

    Clone files from foreign repository

--- plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
+++ plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
@@ -5,15 +5,22 @@ import com.aliucord.entities.Plugin
 import android.content.Context
 import com.aliucord.Utils.showToast
 import com.aliucord.SettingsUtilsJSON
+import org.json.*
+
 import com.discord.stores.StoreStream
 import com.discord.models.user.MeUser
-import org.json.*
+
+import android.view.View
+import java.time.Instant
+import com.discord.databinding.WidgetChatListAdapterItemTextBinding
+import com.discord.utilities.view.text.LinkifiedTextView
 
 @AliucordPlugin(requiresRestart = true)
 class JSOPTest: Plugin(){
 	@SuppressLint("SetTextI18n")
 	val settings2 = SettingsUtilsJSON("JSOPTest");
-	override fun start(pluginContext: Context){
+
+	override fun start(pluginContext: Context){/*
 		val imports = settings.getObject<MutableMap<String, String>>("imports", mutableMapOf());
 		val jsop = JSOP(imports, object{
 			val `$me`: MeUser = StoreStream.getUsers().me;
@@ -27,6 +34,30 @@ class JSOPTest: Plugin(){
 			true-> showToast("Yuore a bot", showLonger = true);
 			else -> showToast("Idk wtf yuore", showLonger = true);
 		};
+*/
+		val viewID = Utils.getResId("chat_list_adapter_item_text", "id");
+		patcher.patch(
+			WidgetChatListAdapterItemTextBinding::class.java
+				.getDeclaredMethod("a", View:class.java)
+			,
+			Hook{frame ->
+				val result = frame.getResult() as WidgetChatListAdapterItemTextBinding;
+				val view: LinkifiedTextView = result.f2335b;
+				val id = view.id;
+				if(id != viewID){
+					logger.debug("failed: "+id+" ≠ "+viewID);
+					return@Hook;
+				};
+				view.setBackgroundColor(4278190080L
+					.or(Instant.now()
+						.toEpochMilli()
+					).rem(16777215)
+					.toInt()
+				);
+				frame.setResult(result);
+			}
+		);
 	};
+
 	override fun stop(pluginContext: Context) = patcher.unpatchAll();
 };
