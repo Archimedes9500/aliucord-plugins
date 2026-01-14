56bf32b01142a42481d20ff390f83b307c0426e1
Archimedes5000 <archimedes9500@gmail.com>
2026-01-14 14:25:04 +0100

    fix

--- plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
+++ plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
@@ -13,7 +13,7 @@ import com.discord.models.user.MeUser
 import android.view.View
 import java.time.Instant
 import com.aliucord.Utils
-import com.aliucord.utils.ViewUtils
+import com.aliucord.utils.ViewUtils.findViewById
 import com.aliucord.patcher.Hook
 
 import androidx.recyclerview.widget.RecyclerView
@@ -43,13 +43,14 @@ class JSOPTest: Plugin(){
 			else -> showToast("Idk wtf yuore", showLonger = true);
 		};
 */
+		val rootID = Utils.getResId("widget_chat_list_adapter_item_text_root", "id");
 		patcher.patch(LayoutInflater::class.java, "inflate", arrayOf(XmlPullParser::class.java, ViewGroup::class.java, Boolean::class.java), object : XC_MethodHook(10000) {
 			override fun afterHookedMethod(param: XC_MethodHook.MethodHookParam) {
 				val recycler: RecyclerView = (param.result as View).findViewById("chat_list_recycler");
 				for(i in 0 until recycler.childCount){
-					val root: ViewGroup = recycler.getChildAt(i);
-					if((root as View).id != Utils.getResId("widget_chat_list_adapter_item_text_root", "id")) continue;
-					val view: TextView = root.findViewById("chat_list_adapter_item_text");
+					val root = recycler.getChildAt(i);
+					if(root.id != rootID) continue;
+					val view: TextView = (root as ViewGroup).findViewById("chat_list_adapter_item_text");
 					view.setBackgroundColor(4278190080L
 						.or(Instant.now()
 							.toEpochMilli()
