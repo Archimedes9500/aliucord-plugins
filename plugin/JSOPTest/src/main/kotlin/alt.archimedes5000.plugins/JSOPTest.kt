fe5d82cb2396f42f425b36f62112c57b3b6de395
GitHub Action <action@github.com>
2026-01-15 01:07:47 +0000

    Clone files from foreign repository

--- plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
+++ plugin/JSOPTest/src/main/kotlin/alt.archimedes5000.plugins/JSOPTest.kt
@@ -52,6 +52,7 @@ class JSOPTest: Plugin(){
 			object: XC_MethodHook(10000){
 				override fun afterHookedMethod(param: XC_MethodHook.MethodHookParam){
 					val root: View = ((param.args[0] as RecyclerView.ViewHolder).itemView).findViewById("widget_chat_list_adapter_item_text_root");
+					if(root == null) return;
 					logger.debug("hooked: "+root.toString());
 					val view: TextView = (root as ViewGroup).findViewById("chat_list_adapter_item_text");
 
