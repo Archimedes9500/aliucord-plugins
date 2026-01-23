2f4fdda38e75530d9868490c93836ccd7edf63e1
Archimedes5000 <archimedes9500@gmail.com>
2026-01-23 21:38:04 +0100

    fix

---
+++ plugin/SettingsBackup/build.gradle.kts
@@ -0,0 +1,9 @@
+version = "0.0"
+description = "Stores discord settings in a plugin settings file (`Discord.json`) that can be easily transferred between Aliucord instances"
+
+aliucord {
+	changelog.set(
+		"""
+		""".trimIndent()
+	)
+}
