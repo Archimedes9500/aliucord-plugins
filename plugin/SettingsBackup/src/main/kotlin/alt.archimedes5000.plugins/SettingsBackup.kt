0198bfd0eb617ea1f7c5bd89a722dcc805f66d72
Archimedes5000 <archimedes9500@gmail.com>
2026-01-29 19:34:57 +0100

    fix

--- plugin/SettingsBackup/src/main/kotlin/alt.archimedes5000.plugins/SettingsBackup.kt
+++ plugin/SettingsBackup/src/main/kotlin/alt.archimedes5000.plugins/SettingsBackup.kt
@@ -191,9 +191,9 @@ class SettingsBackup: Plugin(){
 			patcher.patch(Persister::class.java.getDeclaredMethod("get"), PreHook{frame ->
 				val original = frame.thisObject as Persister<*>;
 				val valueString = backupPersisters[original.getKey()];
-				val currentValue = "{"+fPersisterValue.get(original).toString()+"}";
+				val currentValue = "["+fPersisterValue.get(original).toString()+"]";
 				logger.debug(
-					"asked for persister: ${original.getKey()}\n	replacing value: ${JSONObject(currentValue)}\n	with value: ${valueString?: "null"}"
+					"asked for persister: ${original.getKey()}\n	replacing value: ${JSONArray(currentValue)}\n	with value: ${valueString?: "null"}"
 				);
 				if(valueString != null){
 					frame.result = deserializePersisterValue(valueString, original);
