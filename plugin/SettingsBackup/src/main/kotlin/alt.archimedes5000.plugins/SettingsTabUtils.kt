e16b92ee66c826b5ba11fcf02747497c140fd2e9
Archimedes5000 <archimedes9500@gmail.com>
2026-01-31 23:19:21 +0100

    fix

--- plugin/SettingsBackup/src/main/kotlin/alt.archimedes5000.plugins/SettingsTabUtils.kt
+++ plugin/SettingsBackup/src/main/kotlin/alt.archimedes5000.plugins/SettingsTabUtils.kt
@@ -2,19 +2,11 @@ package alt.archimedes5000.plugins.utils
 
 import com.aliucord.widgets.BottomSheet
 import com.aliucord.entities.Plugin.SettingsTab
-import android.view.View
-import android.os.Bundle
 import java.lang.reflect.*
 
 fun createSettings(tab: BottomSheet): SettingsTab{
 	return SettingsTab(
-		DelegatedBottomSheet(
-			object : BottomSheet(){
-				override fun onViewCreated(view: View, bundle: Bundle?){
-					super.onViewCreated(view, bundle);
-				};
-			}
-		)::class.java as Class<*>,
+		DelegatedBottomSheet::class.java as Class<*>,
 		SettingsTab.Type.BOTTOM_SHEET
 	).withArgs(tab);
 };
