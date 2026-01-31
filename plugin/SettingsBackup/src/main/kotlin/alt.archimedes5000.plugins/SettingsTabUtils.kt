7e0b797e15a220c4be7d701d757f23aee1bb34d6
GitHub Action <action@github.com>
2026-01-31 18:56:14 +0000

    Clone files from foreign repository

--- plugin/SettingsBackup/src/main/kotlin/alt.archimedes5000.plugins/SettingsTabUtils.kt
+++ plugin/SettingsBackup/src/main/kotlin/alt.archimedes5000.plugins/SettingsTabUtils.kt
@@ -1,7 +1,8 @@
-package import alt.archimedes5000.plugins.utils
+package alt.archimedes5000.plugins.utils
 
 import com.aliucord.widgets.BottomSheet
 import com.aliucord.entities.Plugin.SettingsTab
+import java.lang.reflect.*
 
 fun createSettings(tab: BottomSheet): SettingsTab{
 	return SettingsTab(
