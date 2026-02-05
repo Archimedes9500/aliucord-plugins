fdc8cba7d7996663e08b24fc65b34c703c7ffc7e
GitHub Action <action@github.com>
2026-02-05 13:33:31 +0000

    Clone files from foreign repository

--- plugin/ReflectUtilsTest/src/main/java/alt.archimedes5000.plugins/ReflectUtilsTest.java
+++ plugin/ReflectUtilsTest/src/main/java/alt.archimedes5000.plugins/ReflectUtilsTest.java
@@ -36,7 +36,7 @@ public class ReflectUtilsTest extends Plugin{
     public static Object[] args = {number};
 
     private static int test(int number){
-        int result = this.random.nextInt();
+        int result = random.nextInt();
         result = result/number;
         return result;
     };
@@ -44,13 +44,13 @@ public class ReflectUtilsTest extends Plugin{
     public static int iterations = 1_000_000;
 
     public void noReflection(){
-        for(int i = 0; i < this.iterations; i++){
+        for(int i = 0; i < iterations; i++){
             int result = test(number);
         };
     };
     public void noCache(){
-        for(int i = 0; i < this.iterations; i++){
-            Method m = clazz.getDeclaredMethod(methodName, args);
+        for(int i = 0; i < iterations; i++){
+            Method m = clazz.getDeclaredMethod(methodName, argTypes);
             m.setAccessible(true);
             int result = (int) m.invoke(null, number);
         };
@@ -58,24 +58,24 @@ public class ReflectUtilsTest extends Plugin{
     public void withCache(){
         Method m = clazz.getDeclaredMethod(methodName, argTypes);
         m.setAccessible(true);
-        for(int i = 0; i < this.iterations; i++){
+        for(int i = 0; i < iterations; i++){
             int result = (int) m.invoke(null, number);
         };
     };
     public void oldReflectUtils(){
-        for(int i = 0; i < this.iterations; i++){
+        for(int i = 0; i < iterations; i++){
             Method m = oldGetMethodByArgs(clazz, methodName, args);
             int result = (int) m.invoke(null, number);
         };
     };
     public void oldReflectUtilsWithCache(){
         Method m = oldGetMethodByArgs(clazz, methodName, args);
-        for(int i = 0; i < this.iterations; i++){
+        for(int i = 0; i < iterations; i++){
             int result = (int) m.invoke(null, number);
         };
     };
     public void newReflectUtils(){
-        for(int i = 0; i < this.iterations; i++){
+        for(int i = 0; i < iterations; i++){
             Method m = ReflectUtils.getMethodByArgs(clazz, methodName, args);
             int result = (int) m.invoke(null, number);
         };
@@ -83,7 +83,7 @@ public class ReflectUtilsTest extends Plugin{
 
     @Override
     public void start(Context context) throws Throwable{
-        int time = 0;
+        long time;
 
         //warmup
         noReflection();
@@ -94,44 +94,44 @@ public class ReflectUtilsTest extends Plugin{
         newReflectUtils();
 
         //test 1
-        time = System.timeNano();
+        time = System.nanoTime();
         noReflection();
-        logger.debug("1 noReflection 1 000 000: "+(System.timeNano()-time).toString());
-        time = System.timeNano();
+        logger.debug("1 noReflection 1 000 000: "+(System.nanoTime()-time).toString());
+        time = System.nanoTime();
         noCache();
-        logger.debug("1 noCache 1 000 000: "+(System.timeNano()-time).toString());
-        time = System.timeNano();
+        logger.debug("1 noCache 1 000 000: "+(System.nanoTime()-time).toString());
+        time = System.nanoTime();
         withCache();
-        logger.debug("1 withCache 1 000 000: "+(System.timeNano()-time).toString());
-        time = System.timeNano();
+        logger.debug("1 withCache 1 000 000: "+(System.nanoTime()-time).toString());
+        time = System.nanoTime();
         oldReflectUtils();
-        logger.debug("1 oldReflectUtils 1 000 000: "+(System.timeNano()-time).toString());
-        time = System.timeNano();
+        logger.debug("1 oldReflectUtils 1 000 000: "+(System.nanoTime()-time).toString());
+        time = System.nanoTime();
         oldReflectUtilsWithCache();
-        logger.debug("1 oldReflectUtilsWithCache 1 000 000: "+(System.timeNano()-time).toString());
-        time = System.timeNano();
+        logger.debug("1 oldReflectUtilsWithCache 1 000 000: "+(System.nanoTime()-time).toString());
+        time = System.nanoTime();
         newReflectUtils();
-        logger.debug("1 newReflectUtils 1 000 000: "+(System.timeNano()-time).toString());
+        logger.debug("1 newReflectUtils 1 000 000: "+(System.nanoTime()-time).toString());
 
         //test 2
-        time = System.timeNano();
+        time = System.nanoTime();
         noReflection();
-        logger.debug("2 noReflection 1 000 000: "+(System.timeNano()-time).toString());
-        time = System.timeNano();
+        logger.debug("2 noReflection 1 000 000: "+(System.nanoTime()-time).toString());
+        time = System.nanoTime();
         noCache();
-        logger.debug("2 noCache 1 000 000: "+(System.timeNano()-time).toString());
-        time = System.timeNano();
+        logger.debug("2 noCache 1 000 000: "+(System.nanoTime()-time).toString());
+        time = System.nanoTime();
         withCache();
-        logger.debug("2 withCache 1 000 000: "+(System.timeNano()-time).toString());
-        time = System.timeNano();
+        logger.debug("2 withCache 1 000 000: "+(System.nanoTime()-time).toString());
+        time = System.nanoTime();
         oldReflectUtils();
-        logger.debug("2 oldReflectUtils 1 000 000: "+(System.timeNano()-time).toString());
-        time = System.timeNano();
+        logger.debug("2 oldReflectUtils 1 000 000: "+(System.nanoTime()-time).toString());
+        time = System.nanoTime();
         oldReflectUtilsWithCache();
-        logger.debug("2 oldReflectUtilsWithCache 1 000 000: "+(System.timeNano()-time).toString());
-        time = System.timeNano();
+        logger.debug("2 oldReflectUtilsWithCache 1 000 000: "+(System.nanoTime()-time).toString());
+        time = System.nanoTime();
         newReflectUtils();
-        logger.debug("2 newReflectUtils 1 000 000: "+(System.timeNano()-time).toString());
+        logger.debug("2 newReflectUtils 1 000 000: "+(System.nanoTime()-time).toString());
     };
 
     @Override
