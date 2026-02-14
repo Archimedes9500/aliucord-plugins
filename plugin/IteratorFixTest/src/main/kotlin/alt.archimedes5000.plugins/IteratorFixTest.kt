4dd3324f53cabbd144248ac1f7501b496d75040a
Archimedes5000 <archimedes9500@gmail.com>
2026-02-14 23:12:42 +0100

    fix

--- plugin/IteratorFixTest/src/main/kotlin/alt.archimedes5000.plugins/IteratorFixTest.kt
+++ plugin/IteratorFixTest/src/main/kotlin/alt.archimedes5000.plugins/IteratorFixTest.kt
@@ -13,10 +13,22 @@ import android.content.Context
 class IteratorFixTest: Plugin(){
 
 	override fun start(pluginContext: Context){
-		val prog = IntProgression(0, 2, 1);
 
-		val iter = prog.iterator();
-		for(i in iter){
+		val autorange = 0..2;
+		val autorangeIter = autorange.iterator();
+		for(i in autorangeIter){
+			logger.debug("$i");
+		};
+
+		val range = IntRange(0, 2);
+		val rangeIter = range.iterator();
+		for(i in rangeIter){
+			logger.debug("$i");
+		};
+		
+		val prog = IntProgression(0, 2, 1);
+		val progIter = prog.iterator();
+		for(i in progIter){
 			logger.debug("$i");
 		};
 
