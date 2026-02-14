d43ddd762459cc2db30b49356b908b32c44d9f1e
GitHub Action <action@github.com>
2026-02-14 23:59:13 +0000

    Clone files from foreign repository

--- plugin/IteratorFixTest/src/main/kotlin/alt.archimedes5000.plugins/IteratorFixTest.kt
+++ plugin/IteratorFixTest/src/main/kotlin/alt.archimedes5000.plugins/IteratorFixTest.kt
@@ -5,15 +5,17 @@ import android.annotation.SuppressLint
 import com.aliucord.entities.Plugin
 import android.content.Context
 
-typealias d0.t.c0  =  IntIterator
-typealias d0.d0.b = IntProgressionIterator
+import d0.t.c0
+import d0.d0.b
+typealias c0 = IntIterator
+typealias b = IntProgressionIterator
 
 @AliucordPlugin(requiresRestart = true)
 @SuppressLint("SetTextI18n")
 class IteratorFixTest: Plugin(){
 
 	override fun start(pluginContext: Context){
-		
+
 		val prog = IntProgression(0, 2, 1);
 		val progIter = prog.iterator() as IntIterator;
 		for(i in progIter){
