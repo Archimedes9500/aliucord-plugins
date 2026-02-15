92c9d72fbffb5b2e34e7b22deeb86e65dd5ac9ab
Archimedes5000 <archimedes9500@gmail.com>
2026-02-15 03:38:37 +0100

    fix

--- plugin/IteratorFixTest/src/main/kotlin/alt.archimedes5000.plugins/IteratorFixTest.kt
+++ plugin/IteratorFixTest/src/main/kotlin/alt.archimedes5000.plugins/IteratorFixTest.kt
@@ -5,33 +5,13 @@ import android.annotation.SuppressLint
 import com.aliucord.entities.Plugin
 import android.content.Context
 
-import com.aliucord.patcher.instead
-
-//import d0.t.c0 as IntIterator
-//import d0.d0.b as IntProgressionIterator
-
 typealias IntIterator = d0.t.c0;
 typealias IntProgressionIterator = d0.d0.b;
 
 @AliucordPlugin(requiresRestart = true)
 @SuppressLint("SetTextI18n")
 class IteratorFixTest: Plugin(){
-
-	val cIntProgressionIterator = Class.forName("kotlin.ranges.IntProgressionIterator")
-		.getDeclaredConstructor(
-			Int::class.java,
-			Int::class.java,
-			Int::class.java
-		)
-		.apply{isAccessible = true}
-	;
-
 	override fun start(pluginContext: Context){
-/*
-		patcher.instead<IntProgression>("iterator"){frame ->
-			return@instead cIntProgressionIterator.newInstance(first, last, step);
-		};
-*/
 		val prog = IntProgression(0, 2, 1);
 		val progIter = prog.iterator() as IntIterator;
 		for(i in progIter){
@@ -39,9 +19,6 @@ class IteratorFixTest: Plugin(){
 		};
 
 		logger.debug("".trimIndent());
-
 	};
-
 	override fun stop(pluginContext: Context) = patcher.unpatchAll();
-
 };
