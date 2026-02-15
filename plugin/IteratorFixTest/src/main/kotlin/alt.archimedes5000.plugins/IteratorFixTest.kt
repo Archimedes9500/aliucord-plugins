4bf0bac8f27e5343f985a7deea0e1d4b514f14c7
GitHub Action <action@github.com>
2026-02-15 00:28:05 +0000

    Clone files from foreign repository

--- plugin/IteratorFixTest/src/main/kotlin/alt.archimedes5000.plugins/IteratorFixTest.kt
+++ plugin/IteratorFixTest/src/main/kotlin/alt.archimedes5000.plugins/IteratorFixTest.kt
@@ -7,8 +7,8 @@ import android.content.Context
 
 import d0.t.c0
 import d0.d0.b
-typealias c0 = IntIterator
-internal typealias b = IntProgressionIterator
+internal typealias c0 = IntIterator
+//internal typealias b = IntProgressionIterator
 
 @AliucordPlugin(requiresRestart = true)
 @SuppressLint("SetTextI18n")
