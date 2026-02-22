278b1f01449eab95251e419fa0db235c20afa2c0
Archimedes5000 <archimedes9500@gmail.com>
2026-02-22 18:51:21 +0100

    fix

--- plugin/FuckAnimations/src/main/kotlin/alt.archimedes5000.plugins/FuckAnimations.kt
+++ plugin/FuckAnimations/src/main/kotlin/alt.archimedes5000.plugins/FuckAnimations.kt
@@ -6,7 +6,8 @@ import com.aliucord.entities.Plugin
 import android.content.Context
 import com.aliucord.patcher.*
 
-import com.discord.widgets.chat.list.InlineMediaView
+import com.discord.stores.StoreStream
+import com.discord.stores.StoreAccessibility
 import com.discord.stores.StoreUserSettings
 
 typealias IntIterator = d0.t.c0;
@@ -16,9 +17,21 @@ typealias IntProgressionIterator = d0.d0.b;
 @SuppressLint("SetTextI18n")
 class FuckAnimations: Plugin(){
 	override fun start(pluginContext: Context){
+	val fReducedMotionEnabled = StoreAccessibility::class.java
+		.getDeclaredField("reducedMotionEnabled")
+		.apply{isAccessible = true}
+	;
+/*
 		patcher.instead<InlineMediaView>("shouldAutoPlay"){frame ->
 			return@instead true;
 		};
+*/
+		patcher.after<StoreStream>("getAccessibility"){frame ->
+			val store = frame.result as StoreAccessibility;
+			fReducedMotionEnabled.set(store, false);
+			frame.result = store;
+		};
+
 		patcher.before<StoreUserSettings>(
 			"observeIsAnimatedEmojisEnabled",
 			Boolean::class.java
