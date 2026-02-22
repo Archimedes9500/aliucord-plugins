1c34eb34d53521a5425adb842b22a1a64df032ec
Archimedes5000 <archimedes9500@gmail.com>
2026-02-22 18:18:56 +0100

    fix

--- plugin/FuckAnimations/src/main/kotlin/alt.archimedes5000.plugins/FuckAnimations.kt
+++ plugin/FuckAnimations/src/main/kotlin/alt.archimedes5000.plugins/FuckAnimations.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import com.aliucord.patcher.*
 
 import com.discord.widgets.chat.list.InlineMediaView
+import com.discord.stores.StoreUserSettings
 
 typealias IntIterator = d0.t.c0;
 typealias IntProgressionIterator = d0.d0.b;
@@ -17,6 +18,24 @@ class FuckAnimations: Plugin(){
 	override fun start(pluginContext: Context){
 		patcher.instead<InlineMediaView>("shouldAutoPlay"){frame ->
 			return@instead true;
+		};
+		patcher.before<StoreUserSettings>(
+			"observeIsAnimatedEmojisEnabled",
+			Boolean::class.java
+		){frame ->
+			frame.args[0] = false;
+		};
+		patcher.before<StoreUserSettings>(
+			"observeIsAutoPlayGifsEnabled",
+			Boolean::class.java
+		){frame ->
+			frame.args[0] = false;
+		};
+		patcher.before<StoreUserSettings>(
+			"observeStickerAnimationSettings",
+			Boolean::class.java
+		){frame ->
+			frame.args[0] = false;
 		};
 	};
 	override fun stop(pluginContext: Context){
