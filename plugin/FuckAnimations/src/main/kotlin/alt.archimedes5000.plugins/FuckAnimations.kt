8229bbe8566cc091b97294332f9677d8fea8f98f
Archimedes5000 <archimedes9500@gmail.com>
2026-02-23 11:58:52 +0100

    fix

---
+++ plugin/FuckAnimations/src/main/kotlin/alt.archimedes5000.plugins/FuckAnimations.kt
@@ -0,0 +1,66 @@
+package alt.archimedes5000.plugins
+
+import com.aliucord.annotations.AliucordPlugin
+import android.annotation.SuppressLint
+import com.aliucord.entities.Plugin
+import android.content.Context
+import com.aliucord.patcher.*
+
+import com.discord.stores.StoreStream
+import com.discord.stores.StoreAccessibility
+import com.discord.widgets.chat.input.emoji.EmojiPickerViewModel
+
+typealias IntIterator = d0.t.c0;
+typealias IntProgressionIterator = d0.d0.b;
+
+@AliucordPlugin(requiresRestart = true)
+@SuppressLint("SetTextI18n")
+class FuckAnimations: Plugin(){
+	var originalState: Boolean? = null;
+
+	override fun start(pluginContext: Context){
+		val store = StoreStream.Companion!!.getAccessibility();
+		originalState = settings.getBool(
+			"originalState",
+			store.isReducedMotionEnabled()
+		);
+		store.setReducedMotionEnabled(true);
+		//Ignore reduced motion for those cases
+		patcher.before<StoreUserSettings>(
+			"observeIsAnimatedEmojisEnabled",
+			Boolean::class.java
+		){frame ->
+			frame.args[0] = false;
+		};
+		patcher.before<StoreUserSettings>(
+			"observeIsAutoPlayGifsEnabled",
+			Boolean::class.java
+		){frame ->
+			frame.args[0] = false;
+		};
+		patcher.before<StoreUserSettings>(
+			"observeStickerAnimationSettings",
+			Boolean::class.java
+		){frame ->
+			frame.args[0] = false;
+		};
+		//And also this one
+		patcher.before<EmojiPickerViewModel.StoreState.Emoji>(
+			com.discord.models.domain.emoji.EmojiSet::class.java,
+			com.discord.stores.StoreEmoji.EmojiContext::class.java,
+			java.util.LinkedHashMap::class.java,
+			String::class.java,
+			Boolean::class.java,
+			Long::class.java,
+			java.util.Set::class.java
+		){
+			frame.args[4] = true;
+		};
+	};
+	override fun stop(pluginContext: Context){
+		StoreStream.Companion!!.getAccessibility().setReducedMotionEnabled(
+			settings.getBool("originalState", originalState?: false)
+		);
+		patcher.unpatchAll();
+	};
+};
