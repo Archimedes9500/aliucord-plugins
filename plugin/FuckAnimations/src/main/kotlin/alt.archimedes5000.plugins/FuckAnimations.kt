3afdb9cb13dcfe42b242a3aeed21b95f80d96700
GitHub Action <action@github.com>
2026-02-23 11:14:51 +0000

    Clone files from foreign repository

--- plugin/FuckAnimations/src/main/kotlin/alt.archimedes5000.plugins/FuckAnimations.kt
+++ plugin/FuckAnimations/src/main/kotlin/alt.archimedes5000.plugins/FuckAnimations.kt
@@ -8,7 +8,7 @@ import com.aliucord.patcher.*
 
 import com.discord.stores.StoreStream
 import com.discord.stores.StoreAccessibility
-com.discord.stores.StoreUserSettings
+import com.discord.stores.StoreUserSettings
 import com.discord.widgets.chat.input.emoji.EmojiPickerViewModel
 
 typealias IntIterator = d0.t.c0;
