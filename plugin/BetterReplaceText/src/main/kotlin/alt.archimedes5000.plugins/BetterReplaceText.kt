8169f6fc944f70c9692609d19ab04fb974e2e616
Archimedes5000 <archimedes9500@gmail.com>
2026-05-10 01:12:33 +0200

    fix

--- plugin/BetterReplaceText/src/main/kotlin/alt.archimedes5000.plugins/BetterReplaceText.kt
+++ plugin/BetterReplaceText/src/main/kotlin/alt.archimedes5000.plugins/BetterReplaceText.kt
@@ -3,15 +3,14 @@ package alt.archimedes5000.plugins
 import alt.archimedes5000.plugins.utils.*
 import com.aliucord.utils.*
 import com.aliucord.annotations.AliucordPlugin
-import android.annotation.SuppressLint
+//import android.annotation.SuppressLint
 import com.aliucord.entities.Plugin
 import android.content.Context
 
 @AliucordPlugin(requiresRestart = true)
-@SuppressLint("SetTextI18n")
+//@SuppressLint("SetTextI18n")
 class BetterReplaceText: Plugin(){
 
-	//var String.balls: String by ExtField();
 	override fun start(pluginContext: Context){
 		logger.debug("".trimIndent());
 		logger.debug(appContext.toString());
