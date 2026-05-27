50c82396601700ef776394bec1adb655fdde6c9d
Archimedes5000 <archimedes9500@gmail.com>
2026-05-27 20:30:03 +0200

    fix

--- plugin/BetterReplaceText/src/main/kotlin/alt.archimedes5000.plugins/Utils.kt
+++ plugin/BetterReplaceText/src/main/kotlin/alt.archimedes5000.plugins/Utils.kt
@@ -7,7 +7,7 @@ import kotlin.reflect.KProperty;
 import com.github.gfx.util.WeakIdentityHashMap;
 
 typealias IntIterator = d0.t.c0;
-typealias ClosedRange = d0.d0.a;
+typealias ClosedRange<T> = d0.d0.a where T: Comparable<in T>
 typealias IntProgressionIterator = d0.d0.b;
 
 class FakeField<V>(): ReadWriteProperty<Any, V> {
