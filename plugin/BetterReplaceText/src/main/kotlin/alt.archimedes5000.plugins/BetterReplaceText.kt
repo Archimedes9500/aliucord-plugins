5549011d5836c161348da695bad6a397e1be4908
Archimedes5000 <archimedes9500@gmail.com>
2026-05-28 14:52:18 +0200

    fix

--- plugin/BetterReplaceText/src/main/kotlin/alt.archimedes5000.plugins/BetterReplaceText.kt
+++ plugin/BetterReplaceText/src/main/kotlin/alt.archimedes5000.plugins/BetterReplaceText.kt
@@ -11,17 +11,51 @@ import com.discord.stores.StoreMessages;
 import com.discord.stores.StoreMessagesLoader.ChannelChunk;
 import com.discord.models.message.Message;
 
+
+var Message.contentField: String by accessFinalField();
+
 @AliucordPlugin(requiresRestart = true)
 class BetterReplaceText: Plugin(){
 
-	val range1 = 0x00000..0x01900;
-	val PUA = 0x0E000..0x0F8FF;
-/*
-	val range2 = 0x00001..0x2FFFC;
-	val SPUAA = 0xF0000..0xFFFFD;
-	val SPUAB = 0x10000..0x10FFFD;
-*/
-	var Message.contentField: String by accessFinalField();
+	val range1 = 0x000000..0x0018FF;
+	val PUA = 0x00E000..0x00F8FF;
+
+	val range2 = 0x010000..0x01FFFF;
+	val SPUAA = 0x0F0000..0x0FFFFF;
+	
+	val range3 = 0x020000..0x02FFFF;
+	val SPUAB = 0x100000..0x10FFFF;
+
+	fun decode(m: Message){
+		val output = StringBuilder(2000);
+		val s = m.contentField;
+		s.codePoints().forEachOrdered{
+			output.appendCodePoint(
+				when{
+					PUA.contains(it) -> range1.first+(it-PUA.first);
+					SPUAA.contains(it) -> range2.first+(it-SPUAA.first);
+					SPUAB.contains(it) -> range3.first+(it-SPUAB.first);
+					else -> it;
+				}
+			);
+		};
+		m.contentField = output.toString();
+	};
+	fun encode(m: Message){
+		val output = StringBuilder(2000);
+		val s = m.contentField;
+		s.codePoints().forEachOrdered{
+			output.appendCodePoint(
+				when{
+					range1.contains(it) -> PUA.first+(it-range1.first);
+					range2.contains(it) -> SPUAA.first+(it-range2.first);
+					range3.contains(it) -> SPUAB.first+(it-range3.first);
+					else -> it;
+				}
+			);
+		};
+		m.contentField = output.toString();
+	};
 
 	override fun start(pluginContext: Context){
 		patcher.before<StoreMessages>(
@@ -30,17 +64,7 @@ class BetterReplaceText: Plugin(){
 		){frame ->
 			val chunk = frame.args[0] as ChannelChunk;
 			for(m in chunk.messages){
-				val output = StringBuilder(2000);
-				val s = m.contentField;
-				s.codePoints().forEachOrdered{
-					output.appendCodePoint(
-						when{
-							PUA.contains(it) -> range1.first+(it-PUA.first);
-							else -> it;
-						}
-					);
-				};
-				m.contentField = output.toString();
+				decode(m);
 			};
 		};
 	};
