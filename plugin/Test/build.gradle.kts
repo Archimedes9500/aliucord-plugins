70bcbf0569f8e530a37e6fad28e57192c43a09a0
Archimedes5000 <archimedes9500@gmail.com>
2026-06-07 22:59:34 +0200

    test

--- plugin/Test/build.gradle.kts
+++ plugin/Test/build.gradle.kts
@@ -11,14 +11,23 @@ aliucord{
 	);
 };
 
-val temp = configurations.create("temp");
+val _impl = configurations.create("_impl");
 val impl = configurations.getByName("implementation");
 impl.dependencies?.forEach{d ->
-	temp.dependencies.add(d);
+	_impl.dependencies.add(d);
 };
 configurations.remove(impl);
+
+val _compile = configurations.create("_compile");
+val compile = configurations.getByName("compileOnly");
+impl.dependencies?.forEach{d ->
+	_compile.dependencies.add(d);
+};
+configurations.remove(compile);
+
 plugins.apply("scala");
-configurations.getByName("implementation").extendsFrom(temp);
+configurations.getByName("implementation").extendsFrom(_impl);
+configurations.getByName("compileOnly").extendsFrom(_compile);
 
 dependencies{
 	implementation("org.scala-lang:scala-library:2.13.12");
