1bdfc5bbabc05108c879cd226670221760a67fa0
Archimedes5000 <archimedes9500@gmail.com>
2026-06-08 11:55:28 +0200

    fix

--- plugin/Test/build.gradle.kts
+++ plugin/Test/build.gradle.kts
@@ -12,43 +12,21 @@ aliucord{
 	);
 };
 
-val tempMap = mutableMapOf<String, Configuration?>(
-	"implementation" to null,
-	//"api" to null,
-	"compileOnly" to null,
-	"runtimeOnly" to null,
-	"annotationProcessor" to null,
-	//"compileClasspath" to null,
-	//"runtimeClasspath" to null,
-	//"compileOnlyApi" to null,
-	"testImplementation" to null,
-	//"testApi" to null,
-	"testCompileOnly" to null,
-	"testRuntimeOnly" to null,
-	"testAnnotationProcessor" to null,
-	//"testCompileClasspath" to null,
-	//"testRuntimeClasspath" to null,
-	//"testCompileOnlyApi" to null,
-);
-tempMap.entries.forEach{(name, _) ->
-	val backup = configurations.create("_$name");
-	val current = configurations.getByName(name);
-	current.dependencies?.forEach{d ->
-		backup.dependencies.add(d);
-	};
-	configurations.remove(current);
-	tempMap[name] = backup;
-};
-plugins.apply("scala");
-tempMap.entries.forEach{(name, backup) ->
-	configurations.getByName(name).extendsFrom(backup!!);
-};
-
 dependencies{
 	implementation("org.scala-lang:scala-library:2.13.12");
 };
 
-tasks.withType<ScalaCompile>().configureEach{
+val scalaCompileDebug = tasks.register("scalaCompile", ScalaCompile::class.java){
+	source = fileTree("src/main/scala"){
+		include("**/*.scala");
+	};
+	classpath =
+		configurations.getByName("implementation")
+		+configurations.getByName("compileOnly")
+	;
+	destinationDirectory.set(
+		layout.buildDirectory.dir("classes/scala/main")
+	);
 	scalaCompileOptions.additionalParameters = listOf("-g:vars,lines,source");
 };
 
