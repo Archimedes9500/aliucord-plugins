8ccdaeea39f558fb786943f3ee917fa67385b31f
Archimedes5000 <archimedes9500@gmail.com>
2026-07-26 01:37:44 +0200

    fix

--- plugin/SystemProfiles/build.gradle.kts
+++ plugin/SystemProfiles/build.gradle.kts
@@ -1,9 +1,10 @@
-version = "0.0"
-description = "test"
+version = "1.0.0"
+description = "Allows you to see profiles of system message (welcome, poll results, etc.) authors by clicking it's icon."
 
 aliucord {
 	changelog.set(
 		"""
+			**1.0.0** Actually works
 		""".trimIndent()
 	)
 }
