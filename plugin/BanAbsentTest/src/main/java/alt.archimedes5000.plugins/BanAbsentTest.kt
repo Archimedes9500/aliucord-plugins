3d923d1f4ea8a6320e288b80f19c65fa9a490297
Archimedes5000 <archimedes9500@gmail.com>
2026-08-08 22:08:50 +0200

    fix

--- plugin/BanAbsentTest/src/main/java/alt.archimedes5000.plugins/BanAbsentTest.kt
+++ plugin/BanAbsentTest/src/main/java/alt.archimedes5000.plugins/BanAbsentTest.kt
@@ -7,14 +7,18 @@ import com.aliucord.entities.Plugin;
 import android.content.Context;
 import com.aliucord.patcher.*;
 
+import com.discord.stores.StoreStream;
 import com.discord.widgets.user.usersheet.WidgetUserSheetViewModel;
 import com.discord.widgets.user.profile.UserProfileAdminView.ViewState;
 import com.discord.stores.StoreBans;
-import com.discord.stores.StoreStream;
+import com.discord.api.permission.Permission;
 
 @AliucordPlugin(requiresRestart = true)
 class BanAbsentUsers: Plugin(){
 	override fun start(pluginContext: Context){
+		val myId = StoreStream.getUsers().me.id;
+		val storeGuilds = StoreStream.getGuilds();
+
 		patcher.after<WidgetUserSheetViewModel.ViewState.Loaded>(
 			"getAdminViewState"
 		){frame ->
@@ -24,14 +28,25 @@ class BanAbsentUsers: Plugin(){
 			val bans = StoreBans.`access$getBannedUsers$p`(
 				StoreStream.getBans()
 			)?.get(guildId) as? Map<Long, *>;
-			logger.debug(bans?.keys?.joinToString("\n")?: "null");
-			val banned = (bans?.containsKey(user.id) == true);
+			val userBanned = (bans?.containsKey(user.id) == true);
+
+			val meMember = storeGuilds.getMember(guildId, myId);
+			val guildRoles = storeGuilds
+				.`getGuildRolesInternal$app_productionGoogleRelease`()
+				.get(guildId)
+			;
+			val everyonePerms = PermissionUtils.applyEveryone(guildId, guildRoles);
+			val perms = PermissionUtils.applyRoles(
+				meMember,
+				guildRoles,
+				everyonePerms
+			);
+			val canBan = PermissionUtils.can(Permission.BAN_MEMBERS, perms);
+
 			frame.result = state.reconstruct(
-				5 to !banned,
-				11 to !banned
-			).also{
-				logger.debug(it.toString());
-			};
+				5 to (!userBanned && canBan),
+				11 to (!userBanned && canBan)
+			);
 		};
 	};
 	override fun stop(pluginContext: Context){
