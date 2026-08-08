6b55c72ee65d4d516edf134b2a29d9c64121b110
Archimedes5000 <archimedes9500@gmail.com>
2026-08-08 13:49:09 +0200

    fix

--- plugin/BanAbsentTest/src/main/java/alt.archimedes5000.plugins/BanAbsentTest.kt
+++ plugin/BanAbsentTest/src/main/java/alt.archimedes5000.plugins/BanAbsentTest.kt
@@ -7,21 +7,26 @@ import com.aliucord.entities.Plugin;
 import android.content.Context;
 import com.aliucord.patcher.*;
 
+import com.discord.stores.StoreStream;
 import com.discord.widgets.user.profile.UserProfileAdminView;
 import com.discord.widgets.user.profile.UserProfileAdminView.ViewState;
 import com.discord.widgets.user.usersheet.WidgetUserSheetViewModel;
 
 @AliucordPlugin(requiresRestart = true)
 class BanAbsentTest: Plugin(){
+	val storeBans = StoreStream.getBans();
+
 	override fun start(pluginContext: Context){
 		patcher.after<WidgetUserSheetViewModel.ViewState.Loaded>(
 			"getAdminViewState"
 		){frame ->
 			val state = frame.result as ViewState;
 			if(state.isMe || state.isAdminSectionEnabled) return@after;
+
+			val bans = storeBans.`access$getBannedUsers$p`(storeBans)[guildId];
 			frame.result = state.reconstruct(
-				5 to true,
-				11 to true
+				5 to true && user !in bans,
+				11 to true && user !in bans
 			).also{
 				logger.debug(it.toString());
 			};
