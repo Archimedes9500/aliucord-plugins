8518ea5e6ba13a47c9b890d53531033bb74cae03
Archimedes5000 <archimedes9500@gmail.com>
2026-08-12 03:28:50 +0200

    fix

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -100,8 +100,8 @@ subprojects {
 
 		compileOnly(libs.discord)
 		compileOnly(libs.aliucord)
-		//compileOnly(files(stripStdlib.map { it.archiveFile }))
-		compileOnly(libs.kotlin.stdlib)
+		compileOnly(files(stripStdlib.map { it.archiveFile }))
+		//compileOnly(libs.kotlin.stdlib)
 		compileOnly("org.jetbrains.kotlin:kotlin-reflect")
 		compileOnly("com.aliucord:Aliuhook:1.1.4")
 		implementation("com.github.gfx.util:weak-identity-hash-map:2.0.0"){
