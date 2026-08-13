8fa7382e8d892528907422e6ce11f6326b28aa97
Archimedes5000 <archimedes9500@gmail.com>
2026-08-13 20:27:59 +0200

    fix

--- plugin/Template/src/main/java/alt.archimedes5000.plugins/Utils.kt
+++ plugin/Template/src/main/java/alt.archimedes5000.plugins/Utils.kt
@@ -320,6 +320,7 @@ val kotlinReflectAvailable = runCatching{
 }.isSuccess;
 
 fun getArgs(type: KType): List<Class<*>?>?{
+	logger.debug(type.toString());
 	return if(kotlinReflectAvailable){
 		Regex("""^\((.*)\) -> (.*)$""")
 			.find(type.toString(), 0)
