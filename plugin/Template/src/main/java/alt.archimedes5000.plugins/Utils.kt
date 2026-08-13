2469fdf5054db5340f9931cf4bfc84173dde596c
Archimedes5000 <archimedes9500@gmail.com>
2026-08-13 21:22:18 +0200

    fix

--- plugin/Template/src/main/java/alt.archimedes5000.plugins/Utils.kt
+++ plugin/Template/src/main/java/alt.archimedes5000.plugins/Utils.kt
@@ -364,7 +364,7 @@ fun getArgs(type: KType): List<Class<*>?>?{
 		?.get(1)
 		?.run{split(", ")}
 	}?.map{
-		classForKotlinName(it, boxed = false);
+		classOrPrimitiveForName(it, boxed = false);
 	};
 };
 
