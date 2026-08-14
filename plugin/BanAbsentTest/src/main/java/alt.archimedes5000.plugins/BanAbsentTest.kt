f8d24ba57b572d1cfa52694567180756db498bdc
Archimedes5000 <archimedes9500@gmail.com>
2026-08-14 16:22:29 +0200

    fix

--- plugin/BanAbsentTest/src/main/java/alt.archimedes5000.plugins/BanAbsentTest.kt
+++ plugin/BanAbsentTest/src/main/java/alt.archimedes5000.plugins/BanAbsentTest.kt
@@ -7,73 +7,41 @@ import com.aliucord.entities.Plugin;
 import android.content.Context;
 import com.aliucord.patcher.*;
 
-import com.discord.utilities.permissions.PermissionUtils;
-import com.discord.api.role.GuildRole;
-import com.discord.models.member.GuildMember;
-
-import com.discord.stores.StoreStream;
 import com.discord.widgets.user.usersheet.WidgetUserSheetViewModel;
-import com.discord.widgets.user.usersheet.WidgetUserSheetViewModel.StoreState;
 import com.discord.widgets.user.profile.UserProfileAdminView.ViewState;
-import com.discord.stores.StoreBans;
 import com.discord.utilities.permissions.ManageUserContext;
-import com.discord.api.permission.Permission;
+import com.discord.stores.StoreBans;
+import com.discord.stores.StoreStream;
 
 @AliucordPlugin(requiresRestart = true)
 class BanAbsentUsers: Plugin(){
-	var WidgetUserSheetViewModel.ViewState.userContext by
-		FakeField<ManageUserContext>()
-	;
+	val WidgetUserSheetViewModel.guildId: Long by accessField();
+	val WidgetUserSheetViewModel.userId: Long by accessField();
 
 	override fun start(pluginContext: Context){
-		try{
 		patcher.patch(
-			WidgetUserSheetViewModel.ViewState::class.java
+			WidgetUserSheetViewModel::class.java
 			.declaredMethods.filter{it.name == "createAdminViewState"}.single(),
 			Hook{frame ->
-				val state = frame.result as ViewState;
-				logger.debug("onCreate:\n${frame.args[3] as ManageUserContext}");
-				if(state.isMe || state.isAdminSectionEnabled) return@Hook;
+				with(frame.thisObject as WidgetUserSheetViewModel){
+					val state = frame.result as ViewState;
 
-				val currentUserContext = frame.args[3] as ManageUserContext;
-
-				with(frame.thisObject as WidgetUserSheetViewModel.ViewState){
-					if(this !is WidgetUserSheetViewModel.ViewState.Loaded){
-						this.userContext = currentUserContext
-					}else{//may never happen
-						val bans = StoreBans.`access$getBannedUsers$p`(
-							StoreStream.getBans()
-						)?.get(guildId) as? Map<Long, *>;
-						val userBanned = (bans?.containsKey(user.id) == true);
+					if(state.isMe || state.isAdminSectionEnabled) return@Hook;
 	
-						frame.result = state.reconstruct(
-							5 to (!userBanned && userContext.canBan),
-							11 to (!userBanned && userContext.canBan)
-						);
-					};
+					val userContext = frame.args[3] as ManageUserContext;
+
+					val bans = StoreBans.`access$getBannedUsers$p`(
+						StoreStream.getBans()
+					)?.get(guildId) as? Map<Long, *>;
+					val userBanned = (bans?.containsKey(userId) == true);
+
+					frame.result = state.reconstruct(
+						5 to (!userBanned && userContext.canBan),
+						11 to (!userBanned && userContext.canBan)
+					);
 				};
 			}
 		);
-		patcher.after<WidgetUserSheetViewModel.ViewState.Loaded>(
-			"getAdminViewState"
-		){frame ->
-			val state = frame.result as ViewState;
-			logger.debug("onGet:\n${userContext}");
-			if(state.isMe || state.isAdminSectionEnabled) return@after;
-
-			val bans = StoreBans.`access$getBannedUsers$p`(
-				StoreStream.getBans()
-			)?.get(guildId) as? Map<Long, *>;
-			val userBanned = (bans?.containsKey(user.id) == true);
-
-			frame.result = state.reconstruct(
-				5 to (!userBanned && userContext.canBan),
-				11 to (!userBanned && userContext.canBan)
-			);
-		};
-		}catch(e: Exception){
-			logger.error("insane", e);
-		};
 	};
 	override fun stop(pluginContext: Context){
 		patcher.unpatchAll();
