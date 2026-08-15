0ff3cd0caad921535345dabd012b7b3f69c966b8
Archimedes5000 <archimedes9500@gmail.com>
2026-08-15 16:37:57 +0200

    fix

--- plugin/BanAbsentTest/src/main/java/alt.archimedes5000.plugins/BanAbsentTest.kt
+++ plugin/BanAbsentTest/src/main/java/alt.archimedes5000.plugins/BanAbsentTest.kt
@@ -19,7 +19,7 @@ class BanAbsentUsers: Plugin(){
 	val WidgetUserSheetViewModel.guildId: Long by accessField();
 	val WidgetUserSheetViewModel.userId: Long by accessField();
 	val WidgetUserSheetViewModel.storeState: StoreState by accessField("mostRecentStoreState");
-	var WidgetUserSheetViewModel.ViewState.userContext: ManageUserContext by FakeField();
+	var UserProfileAdminView.ViewState.userContext: ManageUserContext by FakeField();
 
 	override fun start(pluginContext: Context){
 		patcher.patch(
@@ -65,6 +65,14 @@ class BanAbsentUsers: Plugin(){
 		){frame ->
 			logger.debug("${System.identityHashCode(frame.result)}\n\n${frame.result.userContext}");
 		};
+/*
+		patcher.after<WidgetUserSheetViewModel>(
+			"configureGuildSection",
+			WidgetUserSheetViewModel.ViewState.Loaded::class.java
+		){frame ->
+			logger.debug("${System.identityHashCode(frame.result)}\n\n${frame.result.userContext}");
+		};
+*/
 	};
 	override fun stop(pluginContext: Context){
 		patcher.unpatchAll();
