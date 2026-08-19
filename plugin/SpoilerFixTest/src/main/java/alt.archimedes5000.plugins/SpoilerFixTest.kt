dd835c8ddd5897f308d6a2f17286dbc8b9c96c30
Archimedes5000 <archimedes9500@gmail.com>
2026-08-19 18:04:45 +0200

    fix

--- plugin/SpoilerFixTest/src/main/java/alt.archimedes5000.plugins/SpoilerFixTest.kt
+++ plugin/SpoilerFixTest/src/main/java/alt.archimedes5000.plugins/SpoilerFixTest.kt
@@ -14,11 +14,15 @@ class SpoilerFixTest: Plugin(){
 	val MessageAttachment.flags: Int by accessField();
 
 	override fun start(pluginContext: Context){
-		patcher.after<MessageAttachment>("h"/*"isSpoiler"*/){frame ->
-			val isSpoiler = frame.result as Boolean;
-			if(isSpoiler) return@after;
-			frame.result = (0 != (flags and 8));
-		};
+		Patcher.addPatch(
+			MessageAttachment::class.java,
+			"h"/*"isSpoiler"*/,
+			Hook{frame ->
+				val isSpoiler = frame.result as Boolean;
+				if(isSpoiler) return@after;
+				frame.result = (0 != (flags and 8));
+			}
+		);
 	};
 	override fun stop(pluginContext: Context){
 		patcher.unpatchAll();
