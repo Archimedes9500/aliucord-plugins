3758a0266255d55ef9f5a28a77e5134ecf22d477
Archimedes5000 <archimedes9500@gmail.com>
2026-08-19 15:47:54 +0200

    fix

---
+++ plugin/SpoilerFixTest/src/main/java/alt.archimedes5000.plugins/SpoilerFixTest.kt
@@ -0,0 +1,24 @@
+package alt.archimedes5000.plugins;
+
+import alt.archimedes5000.plugins.utils.*;
+import com.aliucord.utils.*;
+import com.aliucord.annotations.AliucordPlugin;
+import com.aliucord.entities.Plugin;
+import android.content.Context;
+import com.aliucord.patcher.*;
+
+import com.discord.api.message.attachment.MessageAttachment;
+
+@AliucordPlugin(requiresRestart = true)
+class SpoilerFixTest: Plugin(){
+	override fun start(pluginContext: Context){
+		patcher.after<MessageAttachment>("h"/*"isSpoiler"*/){frame ->
+			val isSpoiler = frame.result as Boolean;
+			if(isSpoiler) return@after;
+			frame.result = (0 != (flags & 8));
+		};
+	};
+	override fun stop(pluginContext: Context){
+		patcher.unpatchAll();
+	};
+};
