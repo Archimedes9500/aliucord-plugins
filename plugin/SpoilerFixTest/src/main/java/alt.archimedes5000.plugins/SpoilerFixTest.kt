ece571a29b43fce7ced4f92d845d189acb047cbd
Archimedes5000 <archimedes9500@gmail.com>
2026-08-26 01:17:17 +0200

    fix

--- plugin/SpoilerFixTest/src/main/java/alt.archimedes5000.plugins/SpoilerFixTest.kt
+++ plugin/SpoilerFixTest/src/main/java/alt.archimedes5000.plugins/SpoilerFixTest.kt
@@ -11,7 +11,7 @@ import com.discord.api.message.attachment.MessageAttachment;
 
 @AliucordPlugin(requiresRestart = true)
 class SpoilerFixTest: Plugin(){
-	val MessageAttachment.flags: Int by accessMethod();
+	val MessageAttachment.flags: Int by accessField();
 
 	override fun start(pluginContext: Context){
 		Patcher.addPatch(MessageAttachment::class.java.getDeclaredMethod("h"/*"isSpoiler"*/),
