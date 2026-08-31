18f54a557154e204210abf38ade08ed09e233511
Archimedes5000 <archimedes9500@gmail.com>
2026-08-31 22:30:48 +0200

    fix

--- plugin/Template/src/main/java/alt.archimedes5000.plugins/Utils.kt
+++ plugin/Template/src/main/java/alt.archimedes5000.plugins/Utils.kt
@@ -50,7 +50,7 @@ class FakeField<V>(): ReadWriteProperty<Any, V>{
 				map[key] as V;
 			};
 		};
-		override operator fun set(key: Any, value: V){
+		operator fun set(key: Any, value: V){
 			if(isStatic){
 				staticValue = value;
 			}else{
