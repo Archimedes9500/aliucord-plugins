a916f14906db21ce3146b9cfb9d125144a8f9902
Archimedes5000 <archimedes9500@gmail.com>
2026-09-09 02:19:11 +0200

    fix

--- plugin/Template/build.gradle.kts
+++ plugin/Template/build.gradle.kts
@@ -11,6 +11,7 @@ aliucord{
 @Suppress("UNCHECKED_CAST")
 val deps = rootProject.extra["deps"] as Map<String, DependencyHandlerScope.() -> Unit>;
 dependencies{
+	deps["dexkit"]!!.invoke(this);
 	file("src/main/java/alt.archimedes5000.plugins/")
 		.listFiles()
 		?.filter{it.isDirectory}
