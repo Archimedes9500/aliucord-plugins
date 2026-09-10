da96564ebc5596bb022edd171a0e4c53dd43c325
Archimedes5000 <archimedes9500@gmail.com>
2026-09-10 15:07:43 +0200

    fix

--- plugin/ASMTest/build.gradle.kts
+++ plugin/ASMTest/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.DependencyHandlerScope;
+
 version = "0.0";
 description = "test";
 
@@ -7,14 +9,16 @@ aliucord{
 		""".trimIndent()
 	);
 };
-/*
+
 @Suppress("UNCHECKED_CAST")
 val deps = rootProject.extra["deps"] as Map<String, DependencyHandlerScope.() -> Unit>;
 dependencies{
 	file("src/main/java/alt.archimedes5000.plugins/")
 		.listFiles()
 		?.filter{it.isDirectory}
-		?.forEach{deps[it.name]?.invoke(this)}
+		?.forEach{
+			println("adding ${it.name}");
+			deps[it.name]?.invoke(this);
+		}
 	;
 };
-*/
